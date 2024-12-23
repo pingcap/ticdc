0ada237814ab5581ae1e06f5fea52f1ce0e9bcfc
dongmen <414110582@qq.com>
2024-12-23 19:36:03 +0800

    debug ci 6
    Signed-off-by: dongmen <414110582@qq.com>

--- pkg/eventservice/main_test.go
+++ pkg/eventservice/main_test.go
@@ -24,7 +24,8 @@ func TestMain(m *testing.M) {
 	opts := []goleak.Option{
 		goleak.IgnoreTopFunction("github.com/pingcap/tiflow/pkg/workerpool.(*worker).run"),
 		goleak.IgnoreTopFunction("sync.runtime_Semacquire"),
-		goleak.IgnoreTopFunction("github.com/godbus/dbus.(*Conn).inWorker"),
+		goleak.IgnoreAnyFunction("github.com/godbus/dbus.(*Conn).Auth"),
+		goleak.IgnoreCurrent(),
 	}
 
 	leakutil.SetUpLeakTest(m, opts...)
