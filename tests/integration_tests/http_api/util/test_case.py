35478d028cde469dd615e7b1ee454a28fb9cc690
dongmen <414110582@qq.com>
2025-01-08 18:21:51 +0800

    test: adjust
    Signed-off-by: dongmen <414110582@qq.com>

--- tests/integration_tests/http_api/util/test_case.py
+++ tests/integration_tests/http_api/util/test_case.py
@@ -196,7 +196,7 @@ def update_changefeed():
     # update fail
     # can't update start_ts
     url = BASE_URL0_V2+"/changefeeds/changefeed-test2"
-    data = json.dumps({"start_ts": 0})
+    data = json.dumps({"start_ts": 1})
     headers = {"Content-Type": "application/json"}
     resp = rq.put(url, data=data, headers=headers)
     assert_status_code(resp, rq.codes.bad_request, url)
