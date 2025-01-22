8976dd2eba57dd94daa194bb32345c4f97e066c8
dongmen <414110582@qq.com>
2025-01-22 10:21:05 +0800

    resolve comments
    Signed-off-by: dongmen <414110582@qq.com>

--- pkg/common/types.go
+++ pkg/common/types.go
@@ -50,9 +50,6 @@ func NewDispatcherID() DispatcherID {
 }
 
 func NewDispatcherIDFromPB(pb *heartbeatpb.DispatcherID) DispatcherID {
-	if pb == nil {
-		return DispatcherID{}
-	}
 	d := DispatcherID{Low: pb.Low, High: pb.High}
 	return d
 }
