23083cbfee73b6f7ebf8f6c263e2bc5e58c84c3f
dongmen <414110582@qq.com>
2025-01-24 14:32:24 +0800

    test: adjust code 3
    Signed-off-by: dongmen <414110582@qq.com>

--- tests/integration_tests/capture_suicide_while_balance_table/run.sh
+++ tests/integration_tests/capture_suicide_while_balance_table/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu
+set -eux
 
 CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source $CUR/../_utils/test_prepare
@@ -57,11 +57,11 @@ function run() {
 
 	target_capture=$capture1_id
 	# find a table that capture2 is replicating
-	one_table_id=$(curl -X GET http://127.0.0.1:8301/api/v2/changefeeds/${changefeed_id}/tables |  jq -r --arg cid "$capture2_id" '.items[] | select(.node_id==$cid) | .table_ids[0]')
+	one_table_id=$(curl -X GET "http://127.0.0.1:8301/api/v2/changefeeds/${changefeed_id}/tables" |  jq -r --arg cid "$capture2_id" '.items[] | select(.node_id==$cid) | .table_ids[0]')
 	if [[ $one_table_id == "null" || $one_table_id == "0" ]]; then
 		# if not found, find a table that capture1 is replicating
 		target_capture=$capture2_id
-		one_table_id=$(curl -X GET http://127.0.0.1:8300/api/v2/changefeeds/${changefeed_id}/tables | jq -r --arg cid "$capture1_id" '.items[] | select(.node_id==$cid) | .table_ids[0]')
+		one_table_id=$(curl -X GET "http://127.0.0.1:8300/api/v2/changefeeds/${changefeed_id}/tables" | jq -r --arg cid "$capture1_id" '.items[] | select(.node_id==$cid) | .table_ids[0]')
 	fi
 	table_query=$(mysql -h${UP_TIDB_HOST} -P${UP_TIDB_PORT} -uroot -e "select table_name from information_schema.tables where tidb_table_id = ${one_table_id}\G")
 	table_name=$(echo $table_query | tail -n 1 | awk '{print $(NF)}')
@@ -69,7 +69,7 @@ function run() {
 
 	# sleep some time to wait global resolved ts forwarded
 	sleep 2
-	curl -X "POST http://127.0.0.1:8300/api/v2/changefeeds/${changefeed_id}/move_table?tableID=${one_table_id}&targetNodeID=${target_capture}"
+	curl -X POST "http://127.0.0.1:8300/api/v2/changefeeds/${changefeed_id}/move_table?tableID=${one_table_id}&targetNodeID=${target_capture}"
 	# sleep some time to wait table balance job is written to etcd
 	sleep 2
 
