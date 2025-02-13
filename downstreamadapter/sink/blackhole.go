bf28abafa2d76839f19c7948f805e7face630e59
nhsmw <nhsmwk@gmail.com>
2025-02-13 17:15:21 +0800

    Update downstreamadapter/sink/blackhole.go
    Co-authored-by: dongmen <20351731+asddongmen@users.noreply.github.com>

--- downstreamadapter/sink/blackhole.go
+++ downstreamadapter/sink/blackhole.go
@@ -45,7 +45,7 @@ func (s *BlackHoleSink) SetTableSchemaStore(tableSchemaStore *util.TableSchemaSt
 
 func (s *BlackHoleSink) AddDMLEvent(event *commonEvent.DMLEvent) {
 	// NOTE: don't change the log, integration test `lossy_ddl` depends on it.
-	// https://github.com/pingcap/ticdc/blob/da834db76e0662ff15ef12645d1f37bfa6506d83/tests/integration_tests/lossy_ddl/run.sh#L23
+	// ref: https://github.com/pingcap/ticdc/blob/da834db76e0662ff15ef12645d1f37bfa6506d83/tests/integration_tests/lossy_ddl/run.sh#L23
 	log.Debug("BlackHoleSink: WriteEvents", zap.Any("dml", event))
 	for _, callback := range event.PostTxnFlushed {
 		callback()
