8a970cd5226dc1bb5c1d5e4f032661a50b9885f6
lidezhu <lidezhu@pingcap.com>
2025-02-20 22:08:18 +0800

    alpha version

--- logservice/eventstore/event_store_test.go
+++ logservice/eventstore/event_store_test.go
@@ -15,15 +15,27 @@ package eventstore
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
+	"os"
+	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/pebble"
 	"github.com/klauspost/compress/zstd"
 	"github.com/pingcap/log"
+	"github.com/pingcap/ticdc/pkg/common"
 	"github.com/pingcap/ticdc/utils/chann"
 	"go.uber.org/zap"
 )
+
+// Helper function to generate random data
+func generateRandomData() []byte {
+	data := make([]byte, 10240)
+	rand.Read(data)
+	return data
+}
 
 func BenchmarkEventStoreWrite(b *testing.B) {
 	encoder, err := zstd.NewWriter(nil)
@@ -36,18 +48,48 @@ func BenchmarkEventStoreWrite(b *testing.B) {
 	dbPath := fmt.Sprintf("/tmp/testdb-%s", b.Name())
 	db, err := pebble.Open(dbPath, newPebbleOptions())
 	if err != nil {
-		log.Fatal("open db failed", zap.Error(err))
+		log.Fatal("open db failed", zap.String("path", dbPath), zap.Error(err))
 	}
 	ch := chann.NewUnlimitedChannel[eventWithCallback, uint64](nil, eventWithCallbackSizer)
 	taskpool := newWriteTaskPool(store, db, ch, writeWorkerNumPerDB)
-	taskpool.run(context.Background())
+	var writeNum atomic.Int32
+	finishCallback := func() {
+		writeNum.Add(1)
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	taskpool.run(ctx)
+	defer cancel()
 	b.ResetTimer()
-	for range b.N {
+	for i := 0; i < b.N; i++ {
+		kvs := make([]common.RawKVEntry, 1000)
+		for j := range kvs {
+			kvs[j].OpType = common.OpTypePut
+			// TODO: This is totally ordered
+			kvs[j].CRTs = uint64(i) + 100
+			kvs[j].StartTs = uint64(i) + 99
+			kvs[j].Key = []byte(fmt.Sprintf("key-%d-%d", i, j))
+			kvs[j].KeyLen = uint32(len(kvs[j].Key))
+			kvs[j].Value = generateRandomData()
+			kvs[j].ValueLen = uint32(len(kvs[j].Value))
+		}
+
 		ch.Push(eventWithCallback{
 			subID:    1,
 			tableID:  100,
 			kvs:      kvs,
 			callback: finishCallback,
 		})
+	}
+
+	// Wait for all writes to complete
+	for writeNum.Load() < int32(b.N) {
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	b.StopTimer()
+
+	db.Close()
+	if err := os.RemoveAll(dbPath); err != nil {
+		log.Fatal("Failed to clean up test database", zap.Error(err))
 	}
 }
