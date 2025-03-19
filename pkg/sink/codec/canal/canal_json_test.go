7c58a17ac4ff891b851ac5a822f32fffad97fc4d
3AceShowHand <jinl1037@hotmail.com>
2025-03-19 17:26:51 +0800

    try to fix the order problem

--- pkg/sink/codec/canal/canal_json_test.go
+++ pkg/sink/codec/canal/canal_json_test.go
@@ -103,62 +103,38 @@ func TestBasicTypes(t *testing.T) {
 	CompareRow(t, tableInfo, row, change)
 }
 
-func TestAllTypes(t *testing.T) {
+func TestIntegerTypes(t *testing.T) {
 	helper := commonEvent.NewEventTestHelper(t)
 	defer helper.Close()
 
 	helper.Tk().MustExec("use test")
-	job := helper.DDL2Job(`create table test.t(
-		a tinyint primary key, b tinyint,
-		c bool, d bool,
-		e smallint, f smallint,
-		g int, h int,
-		i float, j float,
-		k double, l double,
-		m timestamp, n timestamp,
-		o bigint, p bigint,
-		q mediumint, r mediumint,
-		s date, t date,
-		u time, v time,
-		w datetime, x datetime,
-		y year, z year,
-		aa varchar(10), ab varchar(10),
-		ac varbinary(16), ad varbinary(10),
-		ae bit(10), af bit(10),
-		ag json, ah json,
-		ai decimal(10,2), aj decimal(10,2),
-		ak enum('a','b','c'), al enum('a','b','c'),
-		am set('a','b','c'), an set('a','b','c'),
-		ao tinytext, ap tinytext,
-		aq tinyblob, ar tinyblob,
-		as1 mediumtext, at mediumtext,
-		au mediumblob, av mediumblob,
-		aw longtext, ax longtext,
-		ay longblob, az longblob,
-		ba text, bb text,
-		bc blob, bd blob,
-		be char(10), bf char(10),
-		bg binary(10), bh binary(10),
-		bi varbinary(16))`)
 
-	dmlEvent := helper.DML2Event("test", "t", `insert into test.t(
-		a,c,e,g,i,k,m,o,q,s,u,w,y,aa,ac,ae,ag,ai,ak,am,ao,aq,as1,au,aw,ay,ba,bc,be,bg, bi) values (
-			1, true, -1, 123, 153.123,153.123,
-			"1973-12-30 15:30:00",123,123,"2000-01-01","23:59:59",
-			"2015-12-20 23:58:58",1970,"测试",0x0102030405060708090A,81,
-			'{"key1": "value1"}', 129012.12, 'a', 'b', "5rWL6K+VdGV4dA==",
-			0x89504E470D0A1A0A,"5rWL6K+VdGV4dA==",0x4944330300000000,
-			"5rWL6K+VdGV4dA==",0x504B0304140000000800,"5rWL6K+VdGV4dA==",
-			0x255044462D312E34,"Alice",0x0102030405060708090A, x'89504E470D0A1A0A')`)
-	require.NotNil(t, dmlEvent)
-	row, ok := dmlEvent.GetNextRow()
+	createTableDDL := `create table test.t(
+		id int primary key auto_increment,
+--  		a tinyint, b tinyint unsigned,
+--  		c smallint, d smallint unsigned,
+--  		e mediumint, f mediumint unsigned,
+--  		g int, h int unsigned,
+ 		i bigint, j bigint unsigned)`
+
+	job := helper.DDL2Job(createTableDDL)
+	tableInfo := helper.GetTableInfo(job)
+
+	sql := `insert into test.t values(
+		1,
+-- 		-128, 0,
+-- 		-32768, 0,
+-- 		-8388608, 0,
+-- 		-2147483648, 0,
+		-9223372036854775808, 0)`
+	minValues := helper.DML2Event("test", "t", sql)
+	minRow, ok := minValues.GetNextRow()
 	require.True(t, ok)
 
-	tableInfo := helper.GetTableInfo(job)
-	rowEvent := &commonEvent.RowEvent{
+	minValueEvent := &commonEvent.RowEvent{
 		TableInfo:      tableInfo,
 		CommitTs:       1,
-		Event:          row,
+		Event:          minRow,
 		ColumnSelector: columnselector.NewDefaultColumnSelector(),
 		Callback:       func() {},
 	}
@@ -169,7 +145,7 @@ func TestAllTypes(t *testing.T) {
 	encoder, err := NewJSONRowEventEncoder(ctx, codecConfig)
 	require.NoError(t, err)
 
-	err = encoder.AppendRowChangedEvent(ctx, "", rowEvent)
+	err = encoder.AppendRowChangedEvent(ctx, "", minValueEvent)
 	require.NoError(t, err)
 
 	messages := encoder.Build()
@@ -193,8 +169,142 @@ func TestAllTypes(t *testing.T) {
 	require.True(t, ok)
 	require.NotNil(t, change.Row)
 
-	CompareRow(t, tableInfo, row, change)
+	CompareRow(t, tableInfo, minValueEvent.Event, change)
+
+	sql = `insert into test.t values (
+		2,
+--  		127, 255,
+--  		32767, 65535,
+--  		8388607, 16777215,
+--  		2147483647, 4294967295,
+ 		9223372036854775807, 18446744073709551615)`
+	maxValues := helper.DML2Event("test", "t", sql)
+	maxRow, ok := maxValues.GetNextRow()
+
+	maxValueEvent := &commonEvent.RowEvent{
+		TableInfo:      tableInfo,
+		CommitTs:       1,
+		Event:          maxRow,
+		ColumnSelector: columnselector.NewDefaultColumnSelector(),
+		Callback:       func() {},
+	}
+
+	err = encoder.AppendRowChangedEvent(ctx, "", maxValueEvent)
+	require.NoError(t, err)
+
+	messages = encoder.Build()
+	require.Len(t, messages, 1)
+
+	err = decoder.AddKeyValue(messages[0].Key, messages[0].Value)
+	require.NoError(t, err)
+
+	messageType, hasNext, err = decoder.HasNext()
+	require.NoError(t, err)
+	require.True(t, hasNext)
+	require.Equal(t, common.MessageTypeRow, messageType)
+
+	event, err = decoder.NextDMLEvent()
+	require.NoError(t, err)
+
+	change, ok = event.GetNextRow()
+	require.True(t, ok)
+	require.NotNil(t, change.Row)
+
+	CompareRow(t, tableInfo, maxValueEvent.Event, change)
 }
+
+//func TestAllTypes(t *testing.T) {
+//	helper := commonEvent.NewEventTestHelper(t)
+//	defer helper.Close()
+//
+//	helper.Tk().MustExec("use test")
+//	job := helper.DDL2Job(`create table test.t(
+//		a tinyint primary key, b tinyint,
+//		c bool, d bool,
+//		e smallint, f smallint,
+//		g int, h int,
+//		i float, j float,
+//		k double, l double,
+//		m timestamp, n timestamp,
+//		o bigint, p bigint,
+//		q mediumint, r mediumint,
+//		s date, t date,
+//		u time, v time,
+//		w datetime, x datetime,
+//		y year, z year,
+//		aa varchar(10), ab varchar(10),
+//		ac varbinary(16), ad varbinary(10),
+//		ae bit(10), af bit(10),
+//		ag json, ah json,
+//		ai decimal(10,2), aj decimal(10,2),
+//		ak enum('a','b','c'), al enum('a','b','c'),
+//		am set('a','b','c'), an set('a','b','c'),
+//		ao tinytext, ap tinytext,
+//		aq tinyblob, ar tinyblob,
+//		as1 mediumtext, at mediumtext,
+//		au mediumblob, av mediumblob,
+//		aw longtext, ax longtext,
+//		ay longblob, az longblob,
+//		ba text, bb text,
+//		bc blob, bd blob,
+//		be char(10), bf char(10),
+//		bg binary(10), bh binary(10),
+//		bi varbinary(16))`)
+//
+//	dmlEvent := helper.DML2Event("test", "t", `insert into test.t(
+//		a,c,e,g,i,k,m,o,q,s,u,w,y,aa,ac,ae,ag,ai,ak,am,ao,aq,as1,au,aw,ay,ba,bc,be,bg, bi) values (
+//			1, true, -1, 123, 153.123,153.123,
+//			"1973-12-30 15:30:00",123,123,"2000-01-01","23:59:59",
+//			"2015-12-20 23:58:58",1970,"测试",0x0102030405060708090A,81,
+//			'{"key1": "value1"}', 129012.12, 'a', 'b', "5rWL6K+VdGV4dA==",
+//			0x89504E470D0A1A0A,"5rWL6K+VdGV4dA==",0x4944330300000000,
+//			"5rWL6K+VdGV4dA==",0x504B0304140000000800,"5rWL6K+VdGV4dA==",
+//			0x255044462D312E34,"Alice",0x0102030405060708090A, x'89504E470D0A1A0A')`)
+//	require.NotNil(t, dmlEvent)
+//	row, ok := dmlEvent.GetNextRow()
+//	require.True(t, ok)
+//
+//	tableInfo := helper.GetTableInfo(job)
+//	rowEvent := &commonEvent.RowEvent{
+//		TableInfo:      tableInfo,
+//		CommitTs:       1,
+//		Event:          row,
+//		ColumnSelector: columnselector.NewDefaultColumnSelector(),
+//		Callback:       func() {},
+//	}
+//
+//	ctx := context.Background()
+//	codecConfig := common.NewConfig(config.ProtocolCanalJSON)
+//	codecConfig.ContentCompatible = true
+//	encoder, err := NewJSONRowEventEncoder(ctx, codecConfig)
+//	require.NoError(t, err)
+//
+//	err = encoder.AppendRowChangedEvent(ctx, "", rowEvent)
+//	require.NoError(t, err)
+//
+//	messages := encoder.Build()
+//	require.Len(t, messages, 1)
+//
+//	decoder, err := NewCanalJSONDecoder(ctx, codecConfig, nil)
+//	require.NoError(t, err)
+//
+//	err = decoder.AddKeyValue(messages[0].Key, messages[0].Value)
+//	require.NoError(t, err)
+//
+//	messageType, hasNext, err := decoder.HasNext()
+//	require.NoError(t, err)
+//	require.True(t, hasNext)
+//	require.Equal(t, common.MessageTypeRow, messageType)
+//
+//	event, err := decoder.NextDMLEvent()
+//	require.NoError(t, err)
+//
+//	change, ok := event.GetNextRow()
+//	require.True(t, ok)
+//	require.NotNil(t, change.Row)
+//
+//	CompareRow(t, tableInfo, row, change)
+//}
 
 func TestGeneralDMLEvent(t *testing.T) {
 	// columnSelector
