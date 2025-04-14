e9fb3ba7155cdbcfac05446253ec43ac0a078dbe
wk989898 <nhsmwk@gmail.com>
2025-04-14 12:43:16 +0000

    update

--- pkg/sink/codec/common/helper.go
+++ pkg/sink/codec/common/helper.go
@@ -371,39 +371,13 @@ func MustSnapshotQuery(
 }
 
 // MustBinaryLiteralToInt convert bytes into uint64,
-// by follow https://github.com/pingcap/tidb/blob/e3417913f58cdd5a136259b902bf177eaf3aa637/types/binary_literal.go#L105
 func MustBinaryLiteralToInt(bytes []byte) uint64 {
-	bytes = trimLeadingZeroBytes(bytes)
-	length := len(bytes)
-
-	if length > 8 {
+	val, err := types.BinaryLiteral(bytes).ToInt(types.DefaultStmtNoWarningContext)
+	if err != nil {
 		log.Panic("invalid bit value found", zap.ByteString("value", bytes))
 		return math.MaxUint64
 	}
-
-	if length == 0 {
-		return 0
-	}
-
-	// Note: the byte-order is BigEndian.
-	val := uint64(bytes[0])
-	for i := 1; i < length; i++ {
-		val = (val << 8) | uint64(bytes[i])
-	}
 	return val
-}
-
-func trimLeadingZeroBytes(bytes []byte) []byte {
-	if len(bytes) == 0 {
-		return bytes
-	}
-	pos, posMax := 0, len(bytes)-1
-	for ; pos < posMax; pos++ {
-		if bytes[pos] != 0 {
-			break
-		}
-	}
-	return bytes[pos:]
 }
 
 const (
