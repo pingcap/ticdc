afb52c5af352bc47953528698941cfa5ecdca46f
3AceShowHand <jinl1037@hotmail.com>
2025-04-15 17:40:55 +0800

    try to fix the column flag

--- pkg/sink/codec/open/message.go
+++ pkg/sink/codec/open/message.go
@@ -20,6 +20,7 @@ import (
 	"strconv"
 
 	"github.com/pingcap/log"
+	commonType "github.com/pingcap/ticdc/pkg/common"
 	"github.com/pingcap/ticdc/pkg/sink/codec/common"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
 	"github.com/pingcap/tidb/pkg/parser/types"
@@ -205,4 +206,94 @@ func (m *messageRow) decode(data []byte) {
 	if err != nil {
 		log.Panic("decode message row failed", zap.Any("data", data), zap.Error(err))
 	}
+}
+
+const (
+	// binaryFlag means the column charset is binary
+	binaryFlag uint64 = 1 << iota
+
+	// handleKeyFlag means the column is selected as the handle key
+	// The handleKey is chosen by the following rules in the order:
+	// 1. if the table has primary key, it's the handle key.
+	// 2. If the table has not null unique key, it's the handle key.
+	// 3. If the table has no primary key and no not null unique key, it has no handleKey.
+	handleKeyFlag
+
+	// generatedColumnFlag means the column is a generated column
+	generatedColumnFlag
+
+	// primaryKeyFlag means the column is primary key
+	primaryKeyFlag
+
+	// uniqueKeyFlag means the column is unique key
+	uniqueKeyFlag
+
+	// multipleKeyFlag means the column is multiple key
+	multipleKeyFlag
+
+	// nullableFlag means the column is nullable
+	nullableFlag
+
+	// unsignedFlag means the column stores an unsigned integer
+	unsignedFlag
+)
+
+func initColumnFlags(tableInfo *commonType.TableInfo) map[string]uint64 {
+	result := make(map[string]uint64, len(tableInfo.GetColumns()))
+	for _, col := range tableInfo.GetColumns() {
+		var flag uint64
+		if col.GetCharset() == "binary" {
+			flag |= binaryFlag
+		}
+		origin := col.GetFlag()
+		if col.IsGenerated() {
+			flag |= generatedColumnFlag
+		}
+		if mysql.HasUniKeyFlag(origin) {
+			flag |= uniqueKeyFlag
+		}
+		if mysql.HasPriKeyFlag(origin) {
+			flag |= primaryKeyFlag
+			if tableInfo.PKIsHandle() {
+				flag |= handleKeyFlag
+			}
+		}
+		if !mysql.HasNotNullFlag(origin) {
+			flag |= nullableFlag
+		}
+		if mysql.HasMultipleKeyFlag(origin) {
+			flag |= multipleKeyFlag
+		}
+		if mysql.HasUnsignedFlag(origin) {
+			flag |= unsignedFlag
+		}
+		result[col.Name.O] = flag
+	}
+
+	// In TiDB, just as in MySQL, only the first column of an index can be marked as "multiple key" or "unique key",
+	// and only the first column of a unique index may be marked as "unique key".
+	// See https://dev.mysql.com/doc/refman/5.7/en/show-columns.html.
+	// Yet if an index has multiple columns, we would like to easily determine that all those columns are indexed,
+	// which is crucial for the completeness of the information we pass to the downstream.
+	// Therefore, instead of using the MySQL standard,
+	// we made our own decision to mark all columns in an index with the appropriate flag(s).
+	for _, idxInfo := range tableInfo.GetIndices() {
+		for _, idxCol := range idxInfo.Columns {
+			flag := result[idxCol.Name.O]
+			if idxInfo.Primary {
+				flag |= primaryKeyFlag
+			} else if idxInfo.Unique {
+				flag |= uniqueKeyFlag
+			}
+			if len(idxInfo.Columns) > 1 {
+				flag |= multipleKeyFlag
+			}
+			colID := tableInfo.ForceGetColumnIDByName(idxCol.Name.O)
+			if tableInfo.IsHandleKey(colID) {
+				flag |= handleKeyFlag
+			}
+			result[idxCol.Name.O] = flag
+		}
+	}
+	return result
 }
