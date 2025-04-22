9f332797711bb36ee47a9516e295ccf400b9bd11
nhsmw <nhsmwk@gmail.com>
2025-04-22 11:00:58 +0800

    Update parallel_dynamic_stream.go

--- utils/dynstream/parallel_dynamic_stream.go
+++ utils/dynstream/parallel_dynamic_stream.go
@@ -111,7 +111,10 @@ func (s *parallelDynamicStream[A, P, T, D, H]) Push(path P, e T) {
 		s.pathMap.RUnlock()
 	}
 
-	ew := eventWrap[A, P, T, D, H]{
+	if pi.stream.isClosed.Load() {
+		return
+	}
+	pi.stream.in() <- eventWrap[A, P, T, D, H]{
 		event:     e,
 		pathInfo:  pi,
 		paused:    s.handler.IsPaused(e),
@@ -120,7 +123,6 @@ func (s *parallelDynamicStream[A, P, T, D, H]) Push(path P, e T) {
 		timestamp: s.handler.GetTimestamp(e),
 		queueTime: time.Now(),
 	}
-	pi.stream.in() <- ew
 }
 
 func (s *parallelDynamicStream[A, P, T, D, H]) Wake(path P) {
@@ -135,6 +137,10 @@ func (s *parallelDynamicStream[A, P, T, D, H]) Wake(path P) {
 		s.pathMap.RUnlock()
 	}
 
+	if pi.stream.isClosed.Load() {
+		log.Error("stream is closed")
+		return
+	}
 	pi.stream.in() <- eventWrap[A, P, T, D, H]{wake: true, pathInfo: pi}
 }
 
@@ -159,6 +165,9 @@ func (s *parallelDynamicStream[A, P, T, D, H]) AddPath(path P, dest D, as ...Are
 
 	s.setMemControl(pi, as...)
 
+	if pi.stream.isClosed.Load() {
+		return NewAppErrorS(ErrorTypeClosed)
+	}
 	pi.stream.addPath(pi)
 
 	s._statAddPathCount.Add(1)
@@ -182,6 +191,9 @@ func (s *parallelDynamicStream[A, P, T, D, H]) RemovePath(path P) error {
 	delete(s.pathMap.m, path)
 	s.pathMap.Unlock()
 
+	if pi.stream.isClosed.Load() {
+		return NewAppErrorS(ErrorTypeClosed)
+	}
 	pi.stream.in() <- eventWrap[A, P, T, D, H]{pathInfo: pi}
 
 	s._statRemovePathCount.Add(1)
