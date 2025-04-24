21a78f822296afa7fbc50356242c9120ae1734dd
hongyunyan <649330952@qq.com>
2025-04-24 14:32:45 +0800

    update

--- pkg/config/replica_config.go
+++ pkg/config/replica_config.go
@@ -101,8 +101,8 @@ var defaultReplicaConfig = &ReplicaConfig{
 		RegionThreshold:            100_000,
 		WriteKeyThreshold:          0,
 		SplitNumberPerNode:         1,
-		SchedulingTaskCountPerNode: 20,  // TODO: choose a btter value
-		RegionCountPerSpan:         100, // TODO: choose a btter value
+		SchedulingTaskCountPerNode: 10, // TODO: choose a btter value
+		RegionCountPerSpan:         50, // TODO: choose a btter value
 	},
 	Integrity: &integrity.Config{
 		IntegrityCheckLevel:   integrity.CheckLevelNone,
