d06eb648ae0ba0e2c1d8ac260f14e0e25144b9b8
wk989898 <nhsmwk@gmail.com>
2025-04-25 07:10:02 +0000

    Revert "canal proto"
    This reverts commit cae8eb7160b2ee769a9b3645d28764d2fb2939bf.

--- pkg/sink/codec/canal/canal_json_message.go
+++ pkg/sink/codec/canal/canal_json_message.go
@@ -14,8 +14,8 @@
 package canal
 
 import (
-	canal "github.com/pingcap/ticdc/pkg/sink/codec/canal/pb"
 	"github.com/pingcap/ticdc/pkg/sink/codec/common"
+	canal "github.com/pingcap/tiflow/proto/canal"
 )
 
 const tidbWaterMarkType = "TIDB_WATERMARK"
