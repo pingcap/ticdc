ca225e304636035db8cedb98e6d28e02c019b466
dongmen <414110582@qq.com>
2025-04-29 23:36:33 +0800

    Revert "Revert "messaging: don't drop local message""
    This reverts commit 575ace970f02b8cb5d37e202bc40f4c0976f9533.

--- pkg/messaging/local_target.go
+++ pkg/messaging/local_target.go
@@ -16,7 +16,6 @@ package messaging
 import (
 	"sync/atomic"
 
-	. "github.com/pingcap/ticdc/pkg/apperror"
 	"github.com/pingcap/ticdc/pkg/metrics"
 	"github.com/pingcap/ticdc/pkg/node"
 	"github.com/prometheus/client_golang/prometheus"
@@ -78,17 +77,11 @@ func (s *localMessageTarget) recordCongestedMessageError(typeE string) {
 }
 
 func (s *localMessageTarget) sendMsgToChan(ch chan *TargetMessage, msg ...*TargetMessage) error {
-	for i, m := range msg {
+	for _, m := range msg {
 		m.To = s.localId
 		m.From = s.localId
 		m.Sequence = s.sequence.Add(1)
-		select {
-		case ch <- m:
-		default:
-			remains := len(msg) - i
-			s.dropMessageCounter.Add(float64(remains))
-			return AppError{Type: ErrorTypeMessageCongested, Reason: "Send message is congested"}
-		}
+		ch <- m
 	}
 	return nil
 }
