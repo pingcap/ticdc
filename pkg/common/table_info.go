ee9ffb2fde3d6ce9f28c5e36a44bc75ed7d8706b
lidezhu <lidezhu@pingcap.com>
2025-05-22 14:35:19 +0800

    fix

--- pkg/common/table_info.go
+++ pkg/common/table_info.go
@@ -94,10 +94,11 @@ type TableInfo struct {
 }
 
 func (ti *TableInfo) InitPrivateFields() {
-	log.Info("initialize table info", zap.String("table", ti.TableName.String()))
+	log.Info("initialize table info")
 	if ti == nil {
 		return
 	}
+	log.Info("initialize table info", zap.String("table", ti.TableName.String()))
 
 	ti.preSQLs.mutex.Lock()
 	defer ti.preSQLs.mutex.Unlock()
