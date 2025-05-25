01abd0a393e6e1be30b1dfee303bc7a5e3dd74fe
hongyunyan <649330952@qq.com>
2025-05-25 16:39:40 +0800

    update

--- downstreamadapter/dispatcher/dispatcher_block_event.go
+++ downstreamadapter/dispatcher/dispatcher_block_event.go
@@ -37,6 +37,9 @@ func (d *Dispatcher) HandleDispatcherStatus(dispatcherStatus *heartbeatpb.Dispat
 		zap.Stringer("dispatcher", d.id),
 		zap.Any("action", dispatcherStatus.GetAction()),
 		zap.Any("ack", dispatcherStatus.GetAck()))
+	defer func() {
+		log.Info("hyy after handle dispatcher status", zap.Any("dispatcherStatus", dispatcherStatus), zap.Stringer("dispatcher", d.id))
+	}()
 	// Step1: deal with the ack info
 	ack := dispatcherStatus.GetAck()
 	if ack != nil {
@@ -46,6 +49,8 @@ func (d *Dispatcher) HandleDispatcherStatus(dispatcherStatus *heartbeatpb.Dispat
 		}
 		d.cancelResendTask(identifier)
 	}
+
+	log.Info("hyy line 53")
 
 	// Step2: deal with the dispatcher action
 	action := dispatcherStatus.GetAction()
@@ -60,6 +65,9 @@ func (d *Dispatcher) HandleDispatcherStatus(dispatcherStatus *heartbeatpb.Dispat
 			// we have not received the block event, and the action is for the future event, so just ignore
 			return
 		}
+
+		log.Info("hyy line 69")
+
 		if d.blockEventStatus.actionMatchs(action) {
 			log.Info("pending event get the action",
 				zap.Any("action", action),
@@ -97,6 +105,8 @@ func (d *Dispatcher) HandleDispatcherStatus(dispatcherStatus *heartbeatpb.Dispat
 			}
 		}
 
+		log.Info("hyy line 108")
+
 		// Step3: whether the outdate message or not, we need to return message show we have finished the event.
 		d.blockStatusesChan <- &heartbeatpb.TableSpanBlockStatus{
 			ID: d.id.ToPB(),
@@ -107,6 +117,8 @@ func (d *Dispatcher) HandleDispatcherStatus(dispatcherStatus *heartbeatpb.Dispat
 				Stage:       heartbeatpb.BlockStage_DONE,
 			},
 		}
+
+		log.Info("hyy line 121")
 	}
 }
 
