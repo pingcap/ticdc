ff478a9debf17478686a2d531e878c8ae32f423e
hongyunyan <649330952@qq.com>
2025-06-23 12:15:50 +0800

    feat(maintainer): introduce SpanController for span lifecycle and state management, with full test coverage

---
+++ maintainer/span/span_controller.go
@@ -0,0 +1,234 @@
+// Copyright 2024 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package span
+
+import (
+	"context"
+
+	"github.com/pingcap/log"
+	"github.com/pingcap/ticdc/heartbeatpb"
+	"github.com/pingcap/ticdc/maintainer/replica"
+	"github.com/pingcap/ticdc/maintainer/split"
+	"github.com/pingcap/ticdc/pkg/common"
+	commonEvent "github.com/pingcap/ticdc/pkg/common/event"
+	"github.com/pingcap/ticdc/pkg/node"
+	"github.com/pingcap/ticdc/pkg/utils"
+	"github.com/pingcap/ticdc/server/watcher"
+	"go.uber.org/zap"
+)
+
+// Controller manages span lifecycle and replication state
+type Controller struct {
+	replicationDB          *replica.ReplicationDB
+	changefeedID           common.ChangeFeedID
+	nodeManager            *watcher.NodeManager
+	splitter               *split.Splitter
+	enableTableAcrossNodes bool
+}
+
+// NewController creates a new span controller
+func NewController(
+	changefeedID common.ChangeFeedID,
+	ddlSpan *replica.SpanReplication,
+	nodeManager *watcher.NodeManager,
+	splitter *split.Splitter,
+	enableTableAcrossNodes bool,
+) *Controller {
+	return &Controller{
+		replicationDB:          replica.NewReplicaSetDB(changefeedID, ddlSpan, enableTableAcrossNodes),
+		changefeedID:           changefeedID,
+		nodeManager:            nodeManager,
+		splitter:               splitter,
+		enableTableAcrossNodes: enableTableAcrossNodes,
+	}
+}
+
+// AddNewTable adds a new table to the span controller
+// This is a complex business logic method that handles table splitting and span creation
+func (c *Controller) AddNewTable(table commonEvent.Table, startTs uint64) {
+	if c.replicationDB.IsTableExists(table.TableID) {
+		log.Warn("table already add, ignore",
+			zap.String("changefeed", c.changefeedID.Name()),
+			zap.Int64("schema", table.SchemaID),
+			zap.Int64("table", table.TableID))
+		return
+	}
+	span := common.TableIDToComparableSpan(table.TableID)
+	tableSpan := &heartbeatpb.TableSpan{
+		TableID:  table.TableID,
+		StartKey: span.StartKey,
+		EndKey:   span.EndKey,
+	}
+	tableSpans := []*heartbeatpb.TableSpan{tableSpan}
+	if c.enableTableAcrossNodes && len(c.nodeManager.GetAliveNodes()) > 1 {
+		// split the whole table span base on region count if table region count is exceed the limit
+		tableSpans = c.splitter.SplitSpansByRegion(context.Background(), tableSpan)
+	}
+	c.addNewSpans(table.SchemaID, tableSpans, startTs)
+}
+
+// addNewSpans creates new spans for the given schema and table spans
+// This is a complex business logic method that handles span creation
+func (c *Controller) addNewSpans(schemaID int64, tableSpans []*heartbeatpb.TableSpan, startTs uint64) {
+	for _, span := range tableSpans {
+		dispatcherID := common.NewDispatcherID()
+		replicaSet := replica.NewSpanReplication(c.changefeedID, dispatcherID, schemaID, span, startTs)
+		c.replicationDB.AddAbsentReplicaSet(replicaSet)
+	}
+}
+
+// Simple query methods (direct delegation to replicationDB)
+
+// GetTaskByID queries a task by dispatcherID, return nil if not found
+func (c *Controller) GetTaskByID(dispatcherID common.DispatcherID) *replica.SpanReplication {
+	return c.replicationDB.GetTaskByID(dispatcherID)
+}
+
+// GetTasksByTableID get all tasks by table id
+func (c *Controller) GetTasksByTableID(tableID int64) []*replica.SpanReplication {
+	return c.replicationDB.GetTasksByTableID(tableID)
+}
+
+// GetTasksBySchemaID get all tasks by schema id
+func (c *Controller) GetTasksBySchemaID(schemaID int64) []*replica.SpanReplication {
+	return c.replicationDB.GetTasksBySchemaID(schemaID)
+}
+
+// GetAllTasks get all tasks
+func (c *Controller) GetAllTasks() []*replica.SpanReplication {
+	return c.replicationDB.GetAllTasks()
+}
+
+// GetTaskSizeBySchemaID get task size by schema id
+func (c *Controller) GetTaskSizeBySchemaID(schemaID int64) int {
+	return c.replicationDB.GetTaskSizeBySchemaID(schemaID)
+}
+
+// TaskSize get total task size
+func (c *Controller) TaskSize() int {
+	return c.replicationDB.TaskSize()
+}
+
+// GetTaskSizeByNodeID get task size by node id
+func (c *Controller) GetTaskSizeByNodeID(id node.ID) int {
+	return c.replicationDB.GetTaskSizeByNodeID(id)
+}
+
+// IsTableExists check if table exists
+func (c *Controller) IsTableExists(tableID int64) bool {
+	return c.replicationDB.IsTableExists(tableID)
+}
+
+// State management methods (direct delegation to replicationDB)
+
+// UpdateSchemaID will update the schema id of the table, and move the task to the new schema map
+// it called when rename a table to another schema
+func (c *Controller) UpdateSchemaID(tableID, newSchemaID int64) {
+	c.replicationDB.UpdateSchemaID(tableID, newSchemaID)
+}
+
+// UpdateStatus updates the status of a span
+func (c *Controller) UpdateStatus(span *replica.SpanReplication, status *heartbeatpb.TableSpanStatus) {
+	c.replicationDB.UpdateStatus(span, status)
+}
+
+// AddAbsentReplicaSet adds absent replica sets
+func (c *Controller) AddAbsentReplicaSet(spans ...*replica.SpanReplication) {
+	c.replicationDB.AddAbsentReplicaSet(spans...)
+}
+
+// AddSchedulingReplicaSet adds scheduling replica sets
+func (c *Controller) AddSchedulingReplicaSet(span *replica.SpanReplication, targetNodeID node.ID) {
+	c.replicationDB.AddSchedulingReplicaSet(span, targetNodeID)
+}
+
+// AddReplicatingSpan adds replicating span
+func (c *Controller) AddReplicatingSpan(span *replica.SpanReplication) {
+	c.replicationDB.AddReplicatingSpan(span)
+}
+
+// MarkSpanAbsent marks span as absent
+func (c *Controller) MarkSpanAbsent(span *replica.SpanReplication) {
+	c.replicationDB.MarkSpanAbsent(span)
+}
+
+// MarkSpanScheduling marks span as scheduling
+func (c *Controller) MarkSpanScheduling(span *replica.SpanReplication) {
+	c.replicationDB.MarkSpanScheduling(span)
+}
+
+// MarkSpanReplicating marks span as replicating
+func (c *Controller) MarkSpanReplicating(span *replica.SpanReplication) {
+	c.replicationDB.MarkSpanReplicating(span)
+}
+
+// BindSpanToNode binds span to node
+func (c *Controller) BindSpanToNode(old, new node.ID, span *replica.SpanReplication) {
+	c.replicationDB.BindSpanToNode(old, new, span)
+}
+
+// RemoveReplicatingSpan removes replicating span
+func (c *Controller) RemoveReplicatingSpan(span *replica.SpanReplication) {
+	c.replicationDB.RemoveReplicatingSpan(span)
+}
+
+// ForceRemove forces to remove a span
+func (c *Controller) ForceRemove(id common.DispatcherID) {
+	c.replicationDB.ForceRemove(id)
+}
+
+// ReplaceReplicaSet replaces replica set
+func (c *Controller) ReplaceReplicaSet(oldReplications []*replica.SpanReplication, newSpans []*heartbeatpb.TableSpan, checkpointTs uint64) {
+	c.replicationDB.ReplaceReplicaSet(oldReplications, newSpans, checkpointTs)
+}
+
+// Statistics methods (direct delegation to replicationDB)
+
+// GetAbsentSize get absent size
+func (c *Controller) GetAbsentSize() int {
+	return c.replicationDB.GetAbsentSize()
+}
+
+// GetSchedulingSize get scheduling size
+func (c *Controller) GetSchedulingSize() int {
+	return c.replicationDB.GetSchedulingSize()
+}
+
+// GetReplicatingSize get replicating size
+func (c *Controller) GetReplicatingSize() int {
+	return c.replicationDB.GetReplicatingSize()
+}
+
+// GetAbsent get absent spans
+func (c *Controller) GetAbsent() []*replica.SpanReplication {
+	return c.replicationDB.GetAbsent()
+}
+
+// GetScheduling get scheduling spans
+func (c *Controller) GetScheduling() []*replica.SpanReplication {
+	return c.replicationDB.GetScheduling()
+}
+
+// GetReplicating get replicating spans
+func (c *Controller) GetReplicating() []*replica.SpanReplication {
+	return c.replicationDB.GetReplicating()
+}
+
+// addWorkingSpans adds working spans
+func (c *Controller) addWorkingSpans(tableMap utils.Map[*heartbeatpb.TableSpan, *replica.SpanReplication]) {
+	tableMap.Ascend(func(span *heartbeatpb.TableSpan, stm *replica.SpanReplication) bool {
+		c.AddReplicatingSpan(stm)
+		return true
+	})
+}
---
+++ maintainer/span/span_controller_test.go
@@ -0,0 +1,293 @@
+// Copyright 2024 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package span
+
+import (
+	"testing"
+
+	"github.com/pingcap/ticdc/heartbeatpb"
+	"github.com/pingcap/ticdc/maintainer/replica"
+	"github.com/pingcap/ticdc/pkg/common"
+	commonEvent "github.com/pingcap/ticdc/pkg/common/event"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewController(t *testing.T) {
+	changefeedID := common.NewChangeFeedIDWithName("test")
+	ddlDispatcherID := common.NewDispatcherID()
+	ddlSpan := replica.NewWorkingSpanReplication(changefeedID, ddlDispatcherID,
+		common.DDLSpanSchemaID,
+		common.DDLSpan, &heartbeatpb.TableSpanStatus{
+			ID:              ddlDispatcherID.ToPB(),
+			ComponentStatus: heartbeatpb.ComponentState_Working,
+			CheckpointTs:    1,
+		}, "node1")
+
+	controller := NewController(
+		changefeedID,
+		ddlSpan,
+		nil,   // nodeManager
+		nil,   // splitter
+		false, // enableTableAcrossNodes
+	)
+
+	require.NotNil(t, controller)
+	require.Equal(t, changefeedID, controller.changefeedID)
+	require.False(t, controller.enableTableAcrossNodes)
+}
+
+func TestController_AddNewTable(t *testing.T) {
+	changefeedID := common.NewChangeFeedIDWithName("test")
+	ddlDispatcherID := common.NewDispatcherID()
+	ddlSpan := replica.NewWorkingSpanReplication(changefeedID, ddlDispatcherID,
+		common.DDLSpanSchemaID,
+		common.DDLSpan, &heartbeatpb.TableSpanStatus{
+			ID:              ddlDispatcherID.ToPB(),
+			ComponentStatus: heartbeatpb.ComponentState_Working,
+			CheckpointTs:    1,
+		}, "node1")
+
+	controller := NewController(
+		changefeedID,
+		ddlSpan,
+		nil,   // nodeManager
+		nil,   // splitter
+		false, // enableTableAcrossNodes
+	)
+
+	table := commonEvent.Table{
+		SchemaID: 1,
+		TableID:  100,
+	}
+
+	// Test adding a new table
+	controller.AddNewTable(table, 1000)
+
+	// Verify the table was added
+	require.True(t, controller.IsTableExists(table.TableID))
+	require.Equal(t, 2, controller.TaskSize()) // DDL span + new table
+
+	// Test adding the same table again (should be ignored)
+	controller.AddNewTable(table, 2000)
+	require.Equal(t, 2, controller.TaskSize()) // Should still be 2
+}
+
+func TestController_GetTaskByID(t *testing.T) {
+	changefeedID := common.NewChangeFeedIDWithName("test")
+	ddlDispatcherID := common.NewDispatcherID()
+	ddlSpan := replica.NewWorkingSpanReplication(changefeedID, ddlDispatcherID,
+		common.DDLSpanSchemaID,
+		common.DDLSpan, &heartbeatpb.TableSpanStatus{
+			ID:              ddlDispatcherID.ToPB(),
+			ComponentStatus: heartbeatpb.ComponentState_Working,
+			CheckpointTs:    1,
+		}, "node1")
+
+	controller := NewController(
+		changefeedID,
+		ddlSpan,
+		nil,   // nodeManager
+		nil,   // splitter
+		false, // enableTableAcrossNodes
+	)
+
+	// Add a table first
+	table := commonEvent.Table{
+		SchemaID: 1,
+		TableID:  100,
+	}
+	controller.AddNewTable(table, 1000)
+
+	// Get all tasks to find the dispatcher ID
+	tasks := controller.GetAllTasks()
+	require.Len(t, tasks, 2) // DDL span + new table
+
+	// Find the non-DDL task
+	var tableTask *replica.SpanReplication
+	for _, task := range tasks {
+		if task.Span.TableID != 0 { // DDL span has TableID 0
+			tableTask = task
+			break
+		}
+	}
+	require.NotNil(t, tableTask)
+
+	dispatcherID := tableTask.ID
+
+	// Test getting task by ID
+	task := controller.GetTaskByID(dispatcherID)
+	require.NotNil(t, task)
+	require.Equal(t, dispatcherID, task.ID)
+
+	// Test getting non-existent task
+	nonExistentID := common.NewDispatcherID()
+	task = controller.GetTaskByID(nonExistentID)
+	require.Nil(t, task)
+}
+
+func TestController_GetTasksByTableID(t *testing.T) {
+	changefeedID := common.NewChangeFeedIDWithName("test")
+	ddlDispatcherID := common.NewDispatcherID()
+	ddlSpan := replica.NewWorkingSpanReplication(changefeedID, ddlDispatcherID,
+		common.DDLSpanSchemaID,
+		common.DDLSpan, &heartbeatpb.TableSpanStatus{
+			ID:              ddlDispatcherID.ToPB(),
+			ComponentStatus: heartbeatpb.ComponentState_Working,
+			CheckpointTs:    1,
+		}, "node1")
+
+	controller := NewController(
+		changefeedID,
+		ddlSpan,
+		nil,   // nodeManager
+		nil,   // splitter
+		false, // enableTableAcrossNodes
+	)
+
+	// Add a table
+	table := commonEvent.Table{
+		SchemaID: 1,
+		TableID:  100,
+	}
+	controller.AddNewTable(table, 1000)
+
+	// Test getting tasks by table ID
+	tasks := controller.GetTasksByTableID(table.TableID)
+	require.Len(t, tasks, 1)
+	require.Equal(t, table.TableID, tasks[0].Span.TableID)
+
+	// Test getting tasks for non-existent table
+	tasks = controller.GetTasksByTableID(999)
+	require.Len(t, tasks, 0)
+}
+
+func TestController_GetTasksBySchemaID(t *testing.T) {
+	changefeedID := common.NewChangeFeedIDWithName("test")
+	ddlDispatcherID := common.NewDispatcherID()
+	ddlSpan := replica.NewWorkingSpanReplication(changefeedID, ddlDispatcherID,
+		common.DDLSpanSchemaID,
+		common.DDLSpan, &heartbeatpb.TableSpanStatus{
+			ID:              ddlDispatcherID.ToPB(),
+			ComponentStatus: heartbeatpb.ComponentState_Working,
+			CheckpointTs:    1,
+		}, "node1")
+
+	controller := NewController(
+		changefeedID,
+		ddlSpan,
+		nil,   // nodeManager
+		nil,   // splitter
+		false, // enableTableAcrossNodes
+	)
+
+	// Add tables from the same schema
+	table1 := commonEvent.Table{
+		SchemaID: 1,
+		TableID:  100,
+	}
+	table2 := commonEvent.Table{
+		SchemaID: 1,
+		TableID:  101,
+	}
+	controller.AddNewTable(table1, 1000)
+	controller.AddNewTable(table2, 1000)
+
+	// Test getting tasks by schema ID
+	tasks := controller.GetTasksBySchemaID(1)
+	require.Len(t, tasks, 2)
+
+	// Test getting tasks for non-existent schema
+	tasks = controller.GetTasksBySchemaID(999)
+	require.Len(t, tasks, 0)
+}
+
+func TestController_UpdateSchemaID(t *testing.T) {
+	changefeedID := common.NewChangeFeedIDWithName("test")
+	ddlDispatcherID := common.NewDispatcherID()
+	ddlSpan := replica.NewWorkingSpanReplication(changefeedID, ddlDispatcherID,
+		common.DDLSpanSchemaID,
+		common.DDLSpan, &heartbeatpb.TableSpanStatus{
+			ID:              ddlDispatcherID.ToPB(),
+			ComponentStatus: heartbeatpb.ComponentState_Working,
+			CheckpointTs:    1,
+		}, "node1")
+
+	controller := NewController(
+		changefeedID,
+		ddlSpan,
+		nil,   // nodeManager
+		nil,   // splitter
+		false, // enableTableAcrossNodes
+	)
+
+	// Add a table
+	table := commonEvent.Table{
+		SchemaID: 1,
+		TableID:  100,
+	}
+	controller.AddNewTable(table, 1000)
+
+	// Verify initial state
+	tasks := controller.GetTasksBySchemaID(1)
+	require.Len(t, tasks, 1)
+
+	// Update schema ID
+	controller.UpdateSchemaID(table.TableID, 2)
+
+	// Verify the task moved to new schema
+	tasks = controller.GetTasksBySchemaID(1)
+	require.Len(t, tasks, 0)
+
+	tasks = controller.GetTasksBySchemaID(2)
+	require.Len(t, tasks, 1)
+}
+
+func TestController_Statistics(t *testing.T) {
+	changefeedID := common.NewChangeFeedIDWithName("test")
+	ddlDispatcherID := common.NewDispatcherID()
+	ddlSpan := replica.NewWorkingSpanReplication(changefeedID, ddlDispatcherID,
+		common.DDLSpanSchemaID,
+		common.DDLSpan, &heartbeatpb.TableSpanStatus{
+			ID:              ddlDispatcherID.ToPB(),
+			ComponentStatus: heartbeatpb.ComponentState_Working,
+			CheckpointTs:    1,
+		}, "node1")
+
+	controller := NewController(
+		changefeedID,
+		ddlSpan,
+		nil,   // nodeManager
+		nil,   // splitter
+		false, // enableTableAcrossNodes
+	)
+
+	// Initially should have only DDL span
+	require.Equal(t, 1, controller.TaskSize()) // DDL span
+	require.Equal(t, 0, controller.GetAbsentSize())
+	require.Equal(t, 0, controller.GetSchedulingSize())
+	require.Equal(t, 1, controller.GetReplicatingSize()) // DDL span is replicating
+
+	// Add a table
+	table := commonEvent.Table{
+		SchemaID: 1,
+		TableID:  100,
+	}
+	controller.AddNewTable(table, 1000)
+
+	// Should have one absent task plus DDL span
+	require.Equal(t, 2, controller.TaskSize())
+	require.Equal(t, 1, controller.GetAbsentSize())
+	require.Equal(t, 0, controller.GetSchedulingSize())
+	require.Equal(t, 1, controller.GetReplicatingSize()) // Only DDL span is replicating
+}
