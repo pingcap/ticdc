51ac7ba32ba21be41309cc88afab47a0091331a2
hongyunyan <649330952@qq.com>
2025-06-24 11:24:19 +0800

    update

--- maintainer/replica/replication_span_test.go
+++ maintainer/replica/replication_span_test.go
@@ -17,7 +17,6 @@ import (
 	"testing"
 
 	"github.com/pingcap/ticdc/heartbeatpb"
-	"github.com/pingcap/ticdc/maintainer/testutil"
 	"github.com/pingcap/ticdc/pkg/common"
 	"github.com/stretchr/testify/require"
 )
@@ -25,7 +24,7 @@ import (
 func TestUpdateStatus(t *testing.T) {
 	t.Parallel()
 
-	replicaSet := NewSpanReplication(common.NewChangeFeedIDWithName("test"), common.NewDispatcherID(), 1, testutil.GetTableSpanByID(4), 10)
+	replicaSet := NewSpanReplication(common.NewChangeFeedIDWithName("test"), common.NewDispatcherID(), 1, getTableSpanByID(4), 10)
 	replicaSet.UpdateStatus(&heartbeatpb.TableSpanStatus{CheckpointTs: 9})
 	require.Equal(t, uint64(10), replicaSet.status.Load().CheckpointTs)
 	replicaSet.UpdateStatus(&heartbeatpb.TableSpanStatus{CheckpointTs: 11})
@@ -35,7 +34,7 @@ func TestUpdateStatus(t *testing.T) {
 func TestNewRemoveDispatcherMessage(t *testing.T) {
 	t.Parallel()
 
-	replicaSet := NewSpanReplication(common.NewChangeFeedIDWithName("test"), common.NewDispatcherID(), 1, testutil.GetTableSpanByID(4), 10)
+	replicaSet := NewSpanReplication(common.NewChangeFeedIDWithName("test"), common.NewDispatcherID(), 1, getTableSpanByID(4), 10)
 	msg := replicaSet.NewRemoveDispatcherMessage("node1")
 	req := msg.Message[0].(*heartbeatpb.ScheduleDispatcherRequest)
 	require.Equal(t, heartbeatpb.ScheduleAction_Remove, req.ScheduleAction)
@@ -46,7 +45,7 @@ func TestNewRemoveDispatcherMessage(t *testing.T) {
 func TestSpanReplication_NewAddDispatcherMessage(t *testing.T) {
 	t.Parallel()
 
-	replicaSet := NewSpanReplication(common.NewChangeFeedIDWithName("test"), common.NewDispatcherID(), 1, testutil.GetTableSpanByID(4), 10)
+	replicaSet := NewSpanReplication(common.NewChangeFeedIDWithName("test"), common.NewDispatcherID(), 1, getTableSpanByID(4), 10)
 
 	msg, err := replicaSet.NewAddDispatcherMessage("node1")
 	require.Nil(t, err)
@@ -55,4 +54,14 @@ func TestSpanReplication_NewAddDispatcherMessage(t *testing.T) {
 	require.Equal(t, heartbeatpb.ScheduleAction_Create, req.ScheduleAction)
 	require.Equal(t, replicaSet.ID.ToPB(), req.Config.DispatcherID)
 	require.Equal(t, replicaSet.schemaID, req.Config.SchemaID)
+}
+
+// getTableSpanByID returns a mock TableSpan for testing
+func getTableSpanByID(id common.TableID) *heartbeatpb.TableSpan {
+	totalSpan := common.TableIDToComparableSpan(id)
+	return &heartbeatpb.TableSpan{
+		TableID:  totalSpan.TableID,
+		StartKey: totalSpan.StartKey,
+		EndKey:   totalSpan.EndKey,
+	}
 }
