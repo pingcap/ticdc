779aa8f849df232e54d6a939e730199991a2d2c9
lidezhu <lidezhu@pingcap.com>
2025-07-06 19:25:50 +0800

    small fix

--- tests/integration_tests/syncpoint/run.sh
+++ tests/integration_tests/syncpoint/run.sh
@@ -174,7 +174,7 @@ function run() {
 	check_table_exists "testSync.simple2" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
 
 	run_sql "SET GLOBAL tidb_enable_external_ts_read = off;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
-	sleep 60
+	sleep 90
 
 	run_sql "SELECT primary_ts, secondary_ts FROM tidb_cdc.syncpoint_v1;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
 	echo "____________________________________"
