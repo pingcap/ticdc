0e683f20c7f77531822650bce989222298bd38d4
wk989898 <nhsmwk@gmail.com>
2025-07-11 06:44:04 +0000

    debug

--- tests/integration_tests/kafka_column_selector_avro/run.sh
+++ tests/integration_tests/kafka_column_selector_avro/run.sh
@@ -52,7 +52,7 @@ function run() {
 	if [ ! -f ./checksum_checker ]; then
 		GO111MODULE=on go build
 	fi
-
+	sleep 10
 	check_table_exists "test.finishmark" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
 
 	./checksum_checker --upstream-uri "root@tcp(${UP_TIDB_HOST}:${UP_TIDB_PORT})/" --downstream-uri "root@tcp(${DOWN_TIDB_HOST}:${DOWN_TIDB_PORT})/" --databases "test" --config="$CUR/conf/changefeed.toml"
