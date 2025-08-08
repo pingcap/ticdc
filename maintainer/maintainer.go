9dc3131edef609dc481e0b2114a66e13b09739f9
hongyunyan <649330952@qq.com>
2025-08-08 15:16:08 +0800

    update

--- maintainer/maintainer.go
+++ maintainer/maintainer.go
@@ -530,6 +530,12 @@ func (m *Maintainer) onNodeChanged() {
 }
 
 func (m *Maintainer) calCheckpointTs() {
+	start := time.Now()
+	defer func() {
+		log.Info("maintainer calculate checkpointTs cost",
+			zap.String("changefeed", m.id.String()),
+			zap.Duration("duration", time.Since(start)))
+	}()
 	defer m.updateMetrics()
 	if !m.bootstrapped.Load() {
 		log.Warn("can not advance checkpointTs since not bootstrapped",
