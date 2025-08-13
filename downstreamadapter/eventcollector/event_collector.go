a5c5ea80d03ed9e7cd6ee9a94fbc4f6267cf5a78
hongyunyan <649330952@qq.com>
2025-08-13 18:18:59 +0800

    update

--- downstreamadapter/eventcollector/event_collector.go
+++ downstreamadapter/eventcollector/event_collector.go
@@ -373,7 +373,9 @@ func (c *EventCollector) sendDispatcherRequests(ctx context.Context) error {
 				c.dispatcherMessageChan.In() <- req
 				// Sleep a short time to avoid too many requests in a short time.
 				// TODO: requests can to different EventService, so we should improve the logic here.
-				time.Sleep(sleepInterval)
+				if sleepInterval == 1*time.Second {
+					time.Sleep(sleepInterval)
+				}
 			}
 		}
 	}
