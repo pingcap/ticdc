595dc90f62e49bd59c00d3118458d5027269d1ae
hongyunyan <649330952@qq.com>
2025-08-15 10:37:08 +0800

    ignore the status from non-replicating task

--- pkg/scheduler/replica/replication_group.go
+++ pkg/scheduler/replica/replication_group.go
@@ -259,6 +259,24 @@ func (g *replicationGroup[T, R]) GetTaskSizePerNode() map[node.ID]int {
 	return res
 }
 
+func (g *replicationGroup[T, R]) IsReplicating(replica R) bool {
+	// log.Info("isReplicating",
+	// 	zap.String("schedulerID", g.id),
+	// 	zap.String("group", g.groupName),
+	// 	zap.String("replica", replica.GetID().String()),
+	// 	zap.Any("len", g.replicating.Len()),
+	// )
+	// g.replicating.Range(func(_ T, r R) bool {
+	// 	log.Info("in isReplicating list",
+	// 		zap.String("schedulerID", g.id),
+	// 		zap.String("group", g.groupName),
+	// 		zap.String("replica", r.GetID().String()),
+	// 	)
+	// 	return true
+	// })
+	return g.replicating.Find(replica.GetID())
+}
+
 type iMap[T ReplicationID, R Replication[T]] struct {
 	inner sync.Map
 }
