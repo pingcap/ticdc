557b1336cc64d1be7303bda43e28f646fad950d4
wk989898 <nhsmwk@gmail.com>
2025-08-25 08:38:14 +0000

    remove debug log

--- downstreamadapter/dispatchermanager/dispatcher_manager_redo.go
+++ downstreamadapter/dispatchermanager/dispatcher_manager_redo.go
@@ -274,7 +274,6 @@ func (e *DispatcherManager) SetGlobalRedoTs(checkpointTs, resolvedTs uint64) boo
 	if e.redoTableTriggerEventDispatcher != nil {
 		e.redoTableTriggerEventDispatcher.UpdateMeta(checkpointTs, resolvedTs)
 	}
-	log.Error("SetGlobalRedoTs", zap.Any("pre", e.redoGlobalTs.Load()), zap.Any("redoGlobalTs", resolvedTs))
 	return util.CompareAndMonotonicIncrease(&e.redoGlobalTs, resolvedTs)
 }
 
