254a63eaea80ea9beb3428ae3e45431d14555997
hongyunyan <649330952@qq.com>
2025-09-03 21:52:01 +0800

    test

--- tests/integration_tests/run_heavy_it_in_ci.sh
+++ tests/integration_tests/run_heavy_it_in_ci.sh
@@ -30,37 +30,41 @@ group_num=${group#G}
 # 12 CPU cores will be allocated to run each mysql heavy group in CI pipelines.
 mysql_groups=(
 	# G00
-	'generate_column many_pk_or_uk multi_source'
+	'syncpoint syncpoint syncpoint syncpoint'
 	# G01
-	'api_v2 ddl_for_split_tables_with_random_move_table'
+	'syncpoint syncpoint syncpoint syncpoint'
 	# G02
-	'availability ddl_for_split_tables_with_failover'
+	'syncpoint syncpoint syncpoint syncpoint'
 	# G03
-	''
+	'syncpoint syncpoint syncpoint syncpoint'
 	# G04
-	'syncpoint syncpoint_check_ts'
+	'syncpoint syncpoint syncpoint syncpoint'
 	# G05
-	'move_table ddl_for_split_tables_with_merge_and_split'
+	'syncpoint syncpoint syncpoint syncpoint'
 	# G06
-	'cdc ddl_for_split_tables_with_random_merge_and_split'
-	# G07
-	'resolve_lock merge_table'
-	# G08
-	'bank'
-	# G09
-	'drop_many_tables'
-	# G10
-	'default_value http_proxies'
-	# G11
-	'ddl_reentrant force_replicate_table'
-	# G12
-	'tidb_mysql_test ddl_with_random_move_table'
+	'syncpoint syncpoint syncpoint syncpoint'
+	# G01
+	'syncpoint syncpoint syncpoint syncpoint'
+	# G02
+	'syncpoint syncpoint syncpoint syncpoint'
+	# G03
+	'syncpoint syncpoint syncpoint syncpoint'
+	# G04
+	'syncpoint syncpoint syncpoint syncpoint'
+	# G05
+	'syncpoint syncpoint syncpoint syncpoint'
 	# G13
-	'fail_over region_merge'
-	# G14
-	'fail_over_ddl_mix'
-	# G15
-	'fail_over_ddl_mix_with_syncpoint'
+	'syncpoint syncpoint syncpoint syncpoint'
+	# G01
+	'syncpoint syncpoint syncpoint syncpoint'
+	# G02
+	'syncpoint syncpoint syncpoint syncpoint'
+	# G03
+	'syncpoint syncpoint syncpoint syncpoint'
+	# G04
+	'syncpoint syncpoint syncpoint syncpoint'
+	# G05
+	'syncpoint syncpoint syncpoint syncpoint'
 )
 
 # 12 CPU cores will be allocated to run each kafka heavy group in CI pipelines.
