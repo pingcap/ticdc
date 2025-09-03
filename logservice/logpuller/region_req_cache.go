18a0f153ce3db732b36d03555b684387fc20acde
dongmen <414110582@qq.com>
2025-09-03 13:12:10 +0800

    puller: adjust
    Signed-off-by: dongmen <414110582@qq.com>

--- logservice/logpuller/region_req_cache.go
+++ logservice/logpuller/region_req_cache.go
@@ -28,7 +28,7 @@ import (
 
 const (
 	checkStaleRequestInterval = time.Second * 5
-	requestGCLifeTime         = time.Minute * 2
+	requestGCLifeTime         = time.Minute * 10
 	addReqRetryInterval       = time.Millisecond * 1
 	addReqRetryLimit          = 3
 )
