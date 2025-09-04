1be73337c374a637a80b9087710cc482e0c8ba1b
wk989898 <nhsmwk@gmail.com>
2025-09-04 01:51:29 +0000

    fix

--- tests/integration_tests/ddl_for_split_tables/run.sh
+++ tests/integration_tests/ddl_for_split_tables/run.sh
@@ -25,7 +25,7 @@ function prepare() {
 	run_sql_file $CUR/data/pre.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
 	run_sql_file $CUR/data/pre.sql ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
 
-	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 500_
+	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 500
 
 	TOPIC_NAME="ticdc-ddl-split-table-$RANDOM"
 	case $SINK_TYPE in
