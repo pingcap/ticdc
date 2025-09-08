05cb8bb1d75cef4cce22d8fc1157e0f3fa126579
wk989898 <nhsmwk@gmail.com>
2025-09-08 03:42:41 +0000

    debug test

--- tests/integration_tests/fail_over_ddl_mix_with_syncpoint/run.sh
+++ tests/integration_tests/fail_over_ddl_mix_with_syncpoint/run.sh
@@ -188,17 +188,31 @@ main_with_consistent() {
 
 	kill -9 $DDL_PID $DML_PID_1 $DML_PID_2 $DML_PID_3 $DML_PID_4 $DML_PID_5
 
+	cleanup_process $CDC_BINARY
 	# to ensure row changed events have been replicated to TiCDC
 	sleep 10
 	changefeed_id="test"
 	storage_path="file://$WORK_DIR/redo"
 	tmp_download_path=$WORK_DIR/cdc_data/redo/$changefeed_id
-	current_tso=$(run_cdc_cli_tso_query $UP_PD_HOST_1 $UP_PD_PORT_1)
-	ensure 50 check_redo_resolved_ts $changefeed_id $current_tso $storage_path $tmp_download_path/meta
-	cleanup_process $CDC_BINARY
+	rts=$(cdc redo meta --storage="$storage_path" --tmp-dir="$tmp_download_path" | grep -oE "resolved-ts:[0-9]+" | awk -F: '{print $2}')
 
+	sed "s/<placeholder>/$rts/g" $CUR/conf/consistent_diff_config.toml >$WORK_DIR/consistent_diff_config.toml
+
+	cat $WORK_DIR/consistent_diff_config.toml
 	cdc redo apply --tmp-dir="$tmp_download_path/apply" --storage="$storage_path" --sink-uri="mysql://normal:123456@127.0.0.1:3306/" >$WORK_DIR/cdc_redo.log
-	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 100
+	check_sync_diff $WORK_DIR $WORK_DIR/consistent_diff_config.toml
+	
+	# to ensure row changed events have been replicated to TiCDC
+	# sleep 10
+	# changefeed_id="test"
+	# storage_path="file://$WORK_DIR/redo"
+	# tmp_download_path=$WORK_DIR/cdc_data/redo/$changefeed_id
+	# current_tso=$(run_cdc_cli_tso_query $UP_PD_HOST_1 $UP_PD_PORT_1)
+	# ensure 50 check_redo_resolved_ts $changefeed_id $current_tso $storage_path $tmp_download_path/meta
+	# cleanup_process $CDC_BINARY
+
+	# cdc redo apply --tmp-dir="$tmp_download_path/apply" --storage="$storage_path" --sink-uri="mysql://normal:123456@127.0.0.1:3306/" >$WORK_DIR/cdc_redo.log
+	# check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 100
 }
 
 trap stop_tidb_cluster EXIT
