cdb90abcc898f876f2b1b50f5c19f734c50fec99
dongmen <414110582@qq.com>
2025-09-11 15:03:22 +0800

    hact it group
    Signed-off-by: dongmen <414110582@qq.com>

--- tests/integration_tests/run_heavy_it_in_ci.sh
+++ tests/integration_tests/run_heavy_it_in_ci.sh
@@ -30,37 +30,31 @@ group_num=${group#G}
 # 12 CPU cores will be allocated to run each mysql heavy group in CI pipelines.
 mysql_groups=(
 	# G00
-	'generate_column many_pk_or_uk multi_source'
+	'multi_source multi_source'
 	# G01
-	'api_v2 ddl_for_split_tables_with_random_move_table'
+	'multi_source multi_source'
 	# G02
-	'availability ddl_for_split_tables_with_failover'
-	# G03
-	'cdc move_table'
-	# G04
-	'syncpoint syncpoint_check_ts '
-	# G05
-	'ddl_for_split_tables_with_merge_and_split'
-	# G06
-	'ddl_for_split_tables_with_random_merge_and_split'
-	# G07
-	'consistent_partition_table consistent_replicate_gbk consistent_replicate_ddl'
-	# G08
-	'default_value http_proxies bank ddl_for_split_tables_random_schedule'
-	# G09
-	'resolve_lock merge_table drop_many_tables bank'
-	# G10
-	'consistent_replicate_nfs consistent_replicate_storage_file consistent_replicate_storage_file_large_value consistent_replicate_storage_s3'
-	# G11
-	'ddl_reentrant force_replicate_table'
-	# G12
-	'tidb_mysql_test ddl_with_random_move_table'
-	# G13
-	'fail_over region_merge'
-	# G14
-	'fail_over_ddl_mix'
-	# G15
-	'fail_over_ddl_mix_with_syncpoint'
+	'multi_source multi_source'
+	# G01
+	'multi_source multi_source'
+	'multi_source multi_source'
+	# G01
+	'multi_source multi_source'
+	# G02
+	'multi_source multi_source'
+	# G01
+	'multi_source multi_source'
+	'multi_source multi_source'
+	# G01
+	'multi_source multi_source'
+	# G02
+	'multi_source multi_source'
+	# G01
+	'multi_source multi_source'
+	'multi_source multi_source'
+	# G01
+	'multi_source multi_source'
+	# G02
 )
 
 # 12 CPU cores will be allocated to run each kafka heavy group in CI pipelines.
