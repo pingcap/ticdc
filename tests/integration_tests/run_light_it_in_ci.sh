34cbd87bb61dc836cc8d0d0edd3ed1ce0db64e00
hongyunyan <649330952@qq.com>
2025-09-11 11:21:47 +0800

    update

--- tests/integration_tests/run_light_it_in_ci.sh
+++ tests/integration_tests/run_light_it_in_ci.sh
@@ -32,39 +32,38 @@ group_num=${group#G}
 # 4 CPU, 16 Gi memory.
 mysql_groups=(
 	# G00
-	'event_filter charset_gbk changefeed_finish sql_mode changefeed_reconstruct fail_over_ddl_A'
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
 	# G01
-	'common_1 foreign_key changefeed_pause_resume fail_over_ddl_B'
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
 	# G02
-	'new_ci_collation safe_mode savepoint fail_over_ddl_C'
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
 	# G03
-	'capture_suicide_while_balance_table kv_client_stream_reconnect fail_over_ddl_D'
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
 	# G04
 	# multi_capture
-	'ci_collation_compatibility resourcecontrol fail_over_ddl_E'
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
 	# G05
-	'vector simple partition_table fail_over_ddl_F'
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
 	# G06
-	'http_api http_api_tls ddl_wait fail_over_ddl_G'
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
 	# G07
-	'http_api_tls_with_user_auth fail_over_ddl_H'
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
 	# G08
-	'capture_session_done_during_task changefeed_dup_error_restart mysql_sink_retry fail_over_ddl_I'
-	# G09
-	'cdc_server_tips ddl_sequence server_config_compatibility fail_over_ddl_J'
-	# G10
-	'changefeed_error bdr_mode fail_over_ddl_K'
-	# G11
-	'multi_tables_ddl ddl_attributes multi_cdc_cluster fail_over_ddl_L'
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	# G05
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	# G06
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	# G07
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
 	# G12
-	'row_format tiflash multi_rocks fail_over_ddl_M'
-	# G13
-	'cli_tls_with_auth cli_with_auth fail_over_ddl_N'
-	# G14
-	'batch_add_table batch_update_to_no_batch fail_over_ddl_O'
-	# G15
-	# 'split_region changefeed_resume_with_checkpoint_ts autorandom gc_safepoint foreign_key_check ddl_for_split_tables'
-	'split_region autorandom gc_safepoint foreign_key_check ddl_for_split_tables'
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	# G05
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	# G06
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	# G07
+	'ddl_wait ddl_wait ddl_wait ddl_wait'
 )
 
 # Resource allocation for kafka light integration tests in CI pipelines:
