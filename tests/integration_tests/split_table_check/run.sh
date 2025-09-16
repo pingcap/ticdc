957a72ab4732a28a18ac428e9c7f0ebd85764120
hongyunyan <649330952@qq.com>
2025-09-16 17:45:28 +0800

    update

--- tests/integration_tests/split_table_check/run.sh
+++ tests/integration_tests/split_table_check/run.sh
@@ -31,52 +31,100 @@ function prepare() {
 
 	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 500
 
-	TOPIC_NAME="ticdc-split-table-check-$RANDOM"
+	# Round1-specific resources are prepared by helper functions later
+}
+
+
+# Helpers
+start_cdc_only() {
+	cd $WORK_DIR
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix "0" --addr "127.0.0.1:8300"
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix "1" --addr "127.0.0.1:8301"
+}
+
+build_sink_and_create_changefeed() {
+	# args: changefeed_id, config_path, topic_prefix
+	local cf_id=$1
+	local cfg_path=$2
+	local topic_prefix=$3
+
+	TOPIC_NAME="${topic_prefix}-$RANDOM"
 	case $SINK_TYPE in
-	kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&kafka-version=${KAFKA_VERSION}&max-message-bytes=10485760" ;;
-	storage) SINK_URI="file://$WORK_DIR/storage_test/$TOPIC_NAME?protocol=canal-json&enable-tidb-extension=true" ;;
-	pulsar)
-		run_pulsar_cluster $WORK_DIR normal
-		SINK_URI="pulsar://127.0.0.1:6650/$TOPIC_NAME?protocol=canal-json&enable-tidb-extension=true"
-		;;
-	*) SINK_URI="mysql://root:@127.0.0.1:3306/" ;;
+		kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&kafka-version=${KAFKA_VERSION}&max-message-bytes=10485760" ;;
+		storage) SINK_URI="file://$WORK_DIR/storage_test/$TOPIC_NAME?protocol=canal-json&enable-tidb-extension=true" ;;
+		pulsar)
+			run_pulsar_cluster $WORK_DIR normal
+			SINK_URI="pulsar://127.0.0.1:6650/$TOPIC_NAME?protocol=canal-json&enable-tidb-extension=true"
+			;;
+		*) SINK_URI="mysql://root:@127.0.0.1:3306/" ;;
 	esac
 	sleep 10
-	run_cdc_cli changefeed create --sink-uri="$SINK_URI" -c "test" --config="$CUR/conf/$1.toml"
+	run_cdc_cli changefeed create --sink-uri="$SINK_URI" -c "$cf_id" --config="$cfg_path"
+
 	case $SINK_TYPE in
-	kafka) run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&version=${KAFKA_VERSION}&max-message-bytes=10485760" ;;
-	storage) run_storage_consumer $WORK_DIR $SINK_URI "" "" ;;
-	pulsar) run_pulsar_consumer --upstream-uri $SINK_URI ;;
+		kafka) run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&version=${KAFKA_VERSION}&max-message-bytes=10485760" ;;
+		storage) run_storage_consumer $WORK_DIR $SINK_URI "" "" ;;
+		pulsar) run_pulsar_consumer --upstream-uri $SINK_URI ;;
 	esac
 }
 
-main() {
-	prepare changefeed
-	
-	# Execute some normal DDLs and DMLs first (these should work fine)
+run_normal_ops_and_check() {
+	# args: changefeed_id
+	local cf_id=$1
 	echo "Executing normal DDLs and DMLs..."
 	run_sql_file $CUR/data/normal_ddls.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
 	run_sql_file $CUR/data/dmls.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
-	
-	# Wait a bit for the normal operations to complete
 	sleep 10
-	
-	# Check that the changefeed is still running normally
-	ensure 20 check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} test "normal" "null" ""
-	
-	# Now execute the DDL that will break splitable (this should cause an error)
+	ensure 20 check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} $cf_id "normal" "null" ""
+}
+
+run_problematic_and_assert() {
+	# args: changefeed_id, mode
+	# mode: enforce -> expect failed; disabled -> kafka expect normal, others failed
+	local cf_id=$1
+	local mode=$2
 	echo "Executing DDL that will break splitable..."
 	run_sql_file $CUR/data/problematic_ddls.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
+	if [ "$mode" = "enforce" ]; then
+		ensure 20 check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} $cf_id "failed" "splitable" ""
+	else
+		case $SINK_TYPE in
+			kafka)
+				ensure 20 check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} $cf_id "normal" "null" ""
+				;;
+			*)
+				ensure 20 check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} $cf_id "failed" "splitable" ""
+				;;
+		esac
+	fi
+}
+
+round_with_enable_splittable_check() {
+	prepare changefeed
 	
-	# Wait for the error to be detected and check that the changefeed has failed with the expected error
-	ensure 20 check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} test "failed" "splitable" ""
-	
+	build_sink_and_create_changefeed "test" "$CUR/conf/changefeed.toml" "ticdc-split-table-check"
+	run_normal_ops_and_check "test"
+	run_problematic_and_assert "test" "enforce"
+
 	echo "SUCCESS: Changefeed correctly reported splitable violation error"
 	
 	cleanup_process $CDC_BINARY
 }
 
+round_without_enable_splittable_check() {
+	# Start only TiCDC for the second round, reuse existing TiDB cluster.
+	start_cdc_only
+	build_sink_and_create_changefeed "test-disable" "$CUR/conf/changefeed_disable.toml" "ticdc-split-table-check-disable"
+	run_normal_ops_and_check "test-disable"
+	run_problematic_and_assert "test-disable" "disabled"
+
+	echo "[Round2] SUCCESS: Changefeed behaved as expected with splittable check disabled"
+
+	cleanup_process $CDC_BINARY
+}
+
 trap stop_tidb_cluster EXIT
-main
+round_with_enable_splittable_check
+round_without_enable_splittable_check
 check_logs $WORK_DIR
 echo "[$(date)] <<<<<< run test case $TEST_NAME success! >>>>>>"
