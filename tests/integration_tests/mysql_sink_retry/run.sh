84e6b10604e2dd4a0d6897d3bffcc8da02670e29
dongmen <414110582@qq.com>
2025-09-17 15:22:07 +0800

    adjust tests 4
    Signed-off-by: dongmen <414110582@qq.com>

--- tests/integration_tests/mysql_sink_retry/run.sh
+++ tests/integration_tests/mysql_sink_retry/run.sh
@@ -49,10 +49,15 @@ function run() {
 
 	# write the same data in upstream and downstream to trigger duplicate entry error
 	run_sql_file $CUR/data/data.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
+	echo "fizz write data finish"
+
+	sleep 10
 
 	run_sql "CREATE TABLE sink_retry.finish_mark_2 (a int primary key);"
 	sleep 30
 	check_table_exists "sink_retry.finish_mark_2" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 60
+
+
 	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
 
 	check_logs_contains $WORK_DIR "Meet Duplicate Entry Error, retry the dmls in safemode"
