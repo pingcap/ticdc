ad86fa96d6df4f225f060f73a2eee277d112e12c
lidezhu <lidezhu@pingcap.com>
2025-09-18 18:03:32 +0800

    maintainer: fix data race

--- maintainer/maintainer_controller_bootstrap.go
+++ maintainer/maintainer_controller_bootstrap.go
@@ -306,7 +306,7 @@ func (c *Controller) createSpanReplication(spanInfo *heartbeatpb.BootstrapTableS
 		c.changefeedID,
 		common.NewDispatcherIDFromPB(spanInfo.ID),
 		spanInfo.SchemaID,
-		spanInfo.Span,
+		spanInfo.Span.Copy(),
 		status,
 		node,
 	)
