2eb8e7e872c5498cbff9a42ba5eecada0fb1e017
wk989898 <nhsmwk@gmail.com>
2025-09-24 02:46:37 +0000

    update test

--- tests/integration_tests/common_1/data/test_finish.sql
+++ tests/integration_tests/common_1/data/test_finish.sql
@@ -1,9 +1,13 @@
 -- mark finish table
-USE `common_1`;
+USE common_1;
 
+create database common;
 create table a (a bigint primary key, b int);
 create table b like a;
-rename table a to c, b to a, c to b;
+rename table a to common.c, b to a, common.c to b;
+
+insert into a values (1, 2);
+insert into b values (3, 4), (5, 6);
 
 CREATE TABLE finish_mark
 (
