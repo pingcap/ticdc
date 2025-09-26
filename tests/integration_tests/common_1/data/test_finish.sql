d08cf806b4a398e37988e0731d309a80e60b7cd2
wk989898 <nhsmwk@gmail.com>
2025-09-26 08:30:16 +0000

    add more test

--- tests/integration_tests/common_1/data/test_finish.sql
+++ tests/integration_tests/common_1/data/test_finish.sql
@@ -16,6 +16,13 @@ rename table x to z, common.y to x, z to common.y;
 insert into x values (1, 2);
 insert into common.y values (3, 4), (5, 6);
 
+create table test1 (a bigint primary key, b int);
+create table common.test4 (a bigint primary key, b int);
+rename table test1 to common.test2, common.test2 to test3, common.test4 to test1, test3 to common.test4;
+
+insert into test1 values (1, 2);
+insert into common.test4 values (3, 4), (5, 6);
+
 CREATE TABLE finish_mark
 (
     a int primary key
