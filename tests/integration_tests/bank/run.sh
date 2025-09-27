197740e89a6b1acc317e8b0bf6aaa4680f8d5ca6
tenfyzhong <tenfy@tenfy.cn>
2025-09-27 21:22:15 +0800

    test(bank): Use `CDC_BINARY` for downstream CDC server
    - Replaced `$DOWN_CDC` with `$CDC_BINARY` for the downstream CDC server binary path.
    - Standardizes the variable used for the CDC binary across test scripts.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- tests/integration_tests/bank/run.sh
+++ tests/integration_tests/bank/run.sh
@@ -28,8 +28,8 @@ function prepare() {
 	fi
 
 	# the cdc server should be classic mode on downstream
-	run_cdc_server --workdir $WORK_DIR --binary $DOWN_CDC --addr "127.0.0.1:8400" --pd "http://${DOWN_PD_HOST}:${DOWN_PD_PORT}" --logsuffix "down"
-	"$DOWN_CDC" cli changefeed create --sink-uri="blackhole://" -c "changefeed-for-find-finished-ts" --server "http://127.0.0.1:8400"
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8400" --pd "http://${DOWN_PD_HOST}:${DOWN_PD_PORT}" --logsuffix "down"
+	cdc_cli_changefeed create --sink-uri="blackhole://" -c "changefeed-for-find-finished-ts" --server "http://127.0.0.1:8400"
 }
 
 trap stop_tidb_cluster EXIT
