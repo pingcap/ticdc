6e3d538e31a45c9212728658ff521c0af40e1cf0
dongmen <414110582@qq.com>
2025-09-28 16:03:27 +0800

    tests: move ddl_wait to heavy group
    Signed-off-by: dongmen <414110582@qq.com>

--- tests/integration_tests/run_light_it_in_ci.sh
+++ tests/integration_tests/run_light_it_in_ci.sh
@@ -32,42 +32,38 @@ group_num=${group#G}
 # 4 CPU, 16 Gi memory.
 mysql_groups=(
 	# G00
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	'event_filter charset_gbk changefeed_finish sql_mode changefeed_reconstruct fail_over_ddl_A'
 	# G01
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	'common_1 foreign_key changefeed_pause_resume fail_over_ddl_B'
 	# G02
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	'new_ci_collation safe_mode savepoint fail_over_ddl_C'
 	# G03
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	'capture_suicide_while_balance_table kv_client_stream_reconnect fail_over_ddl_D'
 	# G04
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	'multi_capture ci_collation_compatibility resourcecontrol fail_over_ddl_E'
 	# G05
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	'vector simple partition_table fail_over_ddl_F'
 	# G06
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	'http_api http_api_tls fail_over_ddl_G'
 	# G07
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
-	# G01
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
-	# G02
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
-	# G03
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
-	# G04
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
-	# G05
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
-	# G06
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
+	'http_api_tls_with_user_auth fail_over_ddl_H changefeed_update_config'
+	# G08
+	'capture_session_done_during_task changefeed_dup_error_restart mysql_sink_retry fail_over_ddl_I'
+	# G09
+	'cdc_server_tips ddl_sequence server_config_compatibility fail_over_ddl_J'
+	# G10
+	'changefeed_error bdr_mode fail_over_ddl_K split_table_check'
+	# G11
+	'multi_tables_ddl ddl_attributes multi_cdc_cluster fail_over_ddl_L'
+	# G12
+	'row_format tiflash multi_rocks fail_over_ddl_M'
+	# G13
+	'cli_tls_with_auth cli_with_auth fail_over_ddl_N'
 	# G14
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
-	# G01
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
-	# G02
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
-	# G03
-	'ddl_wait ddl_wait ddl_wait ddl_wait'
-)Collapse comment
+	'batch_add_table batch_update_to_no_batch fail_over_ddl_O'
+	# G15
+	'split_region changefeed_resume_with_checkpoint_ts autorandom gc_safepoint foreign_key_check ddl_for_split_tables old_arch_compatibility'
+)
 
 # Resource allocation for kafka light integration tests in CI pipelines:
 # https://github.com/PingCAP-QE/ci/blob/main/pipelines/pingcap/ticdc/latest/pod-pull_cdc_kafka_integration_light.yaml
