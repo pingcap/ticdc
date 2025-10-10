0602373f258312d26dfe3104acb171408f49e736
tenfyzhong <tenfy@tenfy.cn>
2025-10-10 10:20:21 +0800

    test(http_proxies): Adapt to macOS hostname resolution
    - Use `ifconfig` for IP address retrieval on macOS.
    - The `hostname -I` command is not supported on macOS.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- tests/integration_tests/http_proxies/run.sh
+++ tests/integration_tests/http_proxies/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -exu
+export PS4='+$(basename ${BASH_SOURCE}):${LINENO}:'
+set -x
 
 CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source $CUR/../_utils/test_prepare
@@ -9,8 +11,13 @@ CDC_BINARY=cdc.test
 SINK_TYPE=$1
 
 TEST_HOST_LIST=(UP_TIDB_HOST UP_PD_HOST_{1..3} UP_TIKV_HOST_{1..3})
-# FIXME: hostname in macOS doesn't support -I option.
-lan_addrs=($(hostname -I))
+if [[ "$(uname)" == "Darwin" ]]; then
+	# macOS
+	lan_addrs=($(ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}'))
+else
+	# Linux
+	lan_addrs=($(hostname -I))
+fi
 lan_addr=${lan_addrs[0]-"127.0.0.1"}
 export UP_TIDB_HOST=$lan_addr \
 	UP_PD_HOST_1=$lan_addr \
