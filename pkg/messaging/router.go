8e58c2353ebf30d74cafd05f264bc9c99eef839e
hongyunyan <649330952@qq.com>
2025-10-11 17:14:01 +0800

    update

--- pkg/messaging/router.go
+++ pkg/messaging/router.go
@@ -16,6 +16,7 @@ package messaging
 import (
 	"context"
 	"sync"
+	"time"
 
 	"github.com/pingcap/log"
 	"go.uber.org/zap"
@@ -60,9 +61,11 @@ func (r *router) runDispatch(ctx context.Context, out <-chan *TargetMessage) {
 				log.Debug("no handler for message, drop it", zap.Any("msg", msg))
 				continue
 			}
-			//start := time.Now()
+			start := time.Now()
 			err := handler(ctx, msg)
-			//log.Info("hyy router runDispatch", zap.Any("msg", msg), zap.Any("time", time.Since(start)))
+			if time.Since(start) > 100*time.Millisecond {
+				log.Info("hyy router runDispatch", zap.Any("msg", msg), zap.Any("time", time.Since(start)))
+			}
 			if err != nil {
 				log.Error("Handle message failed", zap.Error(err), zap.Any("msg", msg))
 			}
