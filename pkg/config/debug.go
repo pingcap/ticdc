1f9c6abb632714978c74305634457dc1dc45a905
dongmen <414110582@qq.com>
2025-10-16 18:06:55 +0800

    config: add config for enable remote event service
    Signed-off-by: dongmen <414110582@qq.com>

--- pkg/config/debug.go
+++ pkg/config/debug.go
@@ -107,14 +107,16 @@ func NewDefaultSchemaStoreConfig() *SchemaStoreConfig {
 
 // EventServiceConfig represents config for event service
 type EventServiceConfig struct {
-	ScanTaskQueueSize int `toml:"scan-task-queue-size" json:"scan-task-queue-size"`
-	ScanLimitInBytes  int `toml:"scan-limit-in-bytes" json:"scan-limit-in-bytes"`
+	ScanTaskQueueSize        int  `toml:"scan-task-queue-size" json:"scan-task-queue-size"`
+	ScanLimitInBytes         int  `toml:"scan-limit-in-bytes" json:"scan-limit-in-bytes"`
+	EnableRemoteEventService bool `toml:"enable-remote-event-service" json:"enable-remote-event-service"`
 }
 
 // NewDefaultEventServiceConfig return the default event service configuration
 func NewDefaultEventServiceConfig() *EventServiceConfig {
 	return &EventServiceConfig{
-		ScanTaskQueueSize: 1024 * 8,
-		ScanLimitInBytes:  1024 * 1024 * 256, // 256MB
+		ScanTaskQueueSize:        1024 * 8,
+		ScanLimitInBytes:         1024 * 1024 * 256, // 256MB
+		EnableRemoteEventService: true,
 	}
 }
