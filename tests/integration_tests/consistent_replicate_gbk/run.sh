9b2d77b5597e3c5c58d3e89860a63975d4581c95
wk989898 <nhsmwk@gmail.com>
2025-10-27 03:53:59 +0000

    fix test

--- tests/integration_tests/consistent_replicate_gbk/run.sh
+++ tests/integration_tests/consistent_replicate_gbk/run.sh
@@ -34,7 +34,6 @@ function run() {
 	fi
 
 	start_tidb_cluster --workdir $WORK_DIR
-	cd $WORK_DIR
 
 	bin/minio server --address $S3_ENDPOINT "$WORK_DIR/s3" &
 	MINIO_PID=$!
@@ -49,6 +48,7 @@ function run() {
 	done
 	s3cmd --access_key=$MINIO_ACCESS_KEY --secret_key=$MINIO_SECRET_KEY --host=$S3_ENDPOINT --host-bucket=$S3_ENDPOINT --no-ssl mb s3://logbucket
 
+	cd $WORK_DIR
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
 	SINK_URI="mysql://normal:123456@127.0.0.1:3306/"
