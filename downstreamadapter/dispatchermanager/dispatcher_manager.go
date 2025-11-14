6427181d52c187ff1cad5ee19f67ce1c5530a910
hongyunyan <649330952@qq.com>
2025-11-14 17:05:37 +0800

    update

--- downstreamadapter/dispatchermanager/dispatcher_manager.go
+++ downstreamadapter/dispatchermanager/dispatcher_manager.go
@@ -842,6 +842,10 @@ func (e *DispatcherManager) close(removeChangefeed bool) {
 		e.heartBeatTask.Cancel()
 	}
 
+	if e.sharedInfo != nil {
+		e.sharedInfo.GetBlockEventDS().Close()
+	}
+
 	if e.RedoEnable {
 		e.redoSink.Close(removeChangefeed)
 	}
