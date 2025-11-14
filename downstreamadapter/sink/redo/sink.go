8afbd7f208a5bb9d5872590f6d24cf8b6aba5dfd
wk989898 <nhsmwk@gmail.com>
2025-11-14 04:23:48 +0000

    debug

--- downstreamadapter/sink/redo/sink.go
+++ downstreamadapter/sink/redo/sink.go
@@ -127,6 +127,7 @@ func (s *Sink) WriteBlockEvent(event commonEvent.BlockEvent) error {
 
 func (s *Sink) AddDMLEvent(event *commonEvent.DMLEvent) {
 	_ = s.statistics.RecordBatchExecution(func() (int, int64, error) {
+		log.Debug("send message to redo", zap.Any("event", event))
 		for {
 			row, ok := event.GetNextRow()
 			if !ok {
@@ -141,7 +142,8 @@ func (s *Sink) AddDMLEvent(event *commonEvent.DMLEvent) {
 				TableInfo:       event.TableInfo,
 				Callback:        event.PostFlush,
 			})
-			log.Debug("send message to redo", zap.Any("event", event))
+			log.Debug("push row", zap.Any("row", row), zap.Any("StartTs", event.StartTs), zap.Any("CommitTs", event.CommitTs),
+				zap.Any("TableInfo", event.TableInfo))
 		}
 		return int(event.Len()), event.GetSize(), nil
 	})
--- pkg/redo/writer/memory/file_worker.go
+++ pkg/redo/writer/memory/file_worker.go
@@ -249,7 +249,7 @@ func (f *fileWorkerGroup) bgWriteLogs(
 				log.Error("inputCh of redo file worker is closed unexpectedly")
 				return errors.ErrUnexpected.FastGenByArgs("inputCh of redo file worker is closed unexpectedly")
 			}
-			log.Debug("send message to log", zap.Any("event", event))
+			log.Debug("send message to log", zap.Any("event", event.ToRedoLog()))
 			err := f.writeToCache(egCtx, event)
 			if err != nil {
 				return errors.Trace(err)
