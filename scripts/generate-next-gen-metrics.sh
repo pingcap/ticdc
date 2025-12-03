b2f7aa4aba33212abe28514782747e17cac79d74
lidezhu <lidezhu@pingcap.com>
2025-12-03 17:10:34 +0800

    fix check

--- scripts/generate-next-gen-metrics.sh
+++ scripts/generate-next-gen-metrics.sh
@@ -64,13 +64,8 @@ jq '
   .panels |= filter_panels
 ' "$NEXT_GEN_SHARED_FILE" >"$NEXT_GEN_USER_FILE"
 
-<<<<<<< HEAD
-sed -i "s/Test-Cluster-TiCDC-New-Arch/Test-Cluster-TiCDC-New-Arch-KeyspaceName/" "$NEXT_GEN_USER_FILE"
-sed -i "s/YiGL8hBZ0aac/lGT5hED6vqTn/" "$NEXT_GEN_USER_FILE"
-=======
-"$SED_CMD" "${SED_INPLACE_ARGS[@]}" "s/\${DS_TEST-CLUSTER}-TiCDC-New-Arch/&-KeyspaceName/" "$NEXT_GEN_USER_FILE"
-"$SED_CMD" "${SED_INPLACE_ARGS[@]}" "s/YiGL8hBZ0aac/lGT5hED6vqTn/" "$NEXT_GEN_USER_FILE"
->>>>>>> f0d4d6f5f (metrics: add more metrics and improve grafana (#3430))
+"$SED_CMD" -i "s/{DS_TEST-CLUSTER}-TiCDC-New-Arch/Test-Cluster-TiCDC-New-Arch-KeyspaceName/" "$NEXT_GEN_USER_FILE"
+"$SED_CMD" -i "s/YiGL8hBZ0aac/lGT5hED6vqTn/" "$NEXT_GEN_USER_FILE"
 
 echo "Userscope dashboard created at '$NEXT_GEN_USER_FILE'"
 
