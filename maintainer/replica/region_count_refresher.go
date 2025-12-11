d05b0aa4d965ac478360ae54ec7d77164ce7d0ed
3AceShowHand <jinl1037@hotmail.com>
2025-12-11 16:24:57 +0800

    add a new refresher implementation

--- maintainer/replica/region_count_refresher.go
+++ maintainer/replica/region_count_refresher.go
@@ -18,169 +18,80 @@ import (
 	"sync"
 	"time"
 
+	"github.com/pingcap/log"
 	"github.com/pingcap/ticdc/heartbeatpb"
 	"github.com/pingcap/ticdc/maintainer/split"
 	"github.com/pingcap/ticdc/pkg/common"
 	"github.com/tikv/client-go/v2/tikv"
+	"go.uber.org/zap"
 )
 
-var defaultRegionCountRefreshInterval = 2 * time.Minute
+type regionCountRefresher struct {
+	regionCache split.RegionCache
+	interval    time.Duration
 
-type regionCountJob struct {
-	id   common.DispatcherID
-	span *heartbeatpb.TableSpan
+	traced sync.Map // map[common.DispatcherID]*heartbeatpb.TableSpan
+	counts sync.Map // map[common.DispatcherID]int
 }
 
-func queryRegionCount(regionCache split.RegionCache, job regionCountJob) (int, error) {
-	regions, err := regionCache.LoadRegionsInKeyRange(
-		tikv.NewBackoffer(context.Background(), 500),
-		job.span.StartKey, job.span.EndKey,
-	)
-	if err != nil {
-		return 0, err
-	}
-	return len(regions), nil
-}
-
-func runRegionCountJobs(regionCache split.RegionCache, jobs []regionCountJob, apply func(regionCountJob, int, error)) {
-	for _, job := range jobs {
-		count, err := queryRegionCount(regionCache, job)
-		apply(job, count, err)
+func newRegionCountRefresher(regionCache split.RegionCache, interval time.Duration) *regionCountRefresher {
+	return &regionCountRefresher{
+		regionCache: regionCache,
+		interval:    interval,
 	}
 }
 
-var splitRegionRefresher = newSplitRegionCountManager()
-
-type splitRegionCountManager struct {
-	mu      sync.Mutex
-	workers map[string]*splitRegionCountWorker
+func (r *regionCountRefresher) addDispatcher(id common.DispatcherID, span *heartbeatpb.TableSpan) {
+	r.traced.Store(id, span)
 }
 
-func newSplitRegionCountManager() *splitRegionCountManager {
-	return &splitRegionCountManager{
-		workers: make(map[string]*splitRegionCountWorker),
-	}
+func (r *regionCountRefresher) removeDispatcher(id common.DispatcherID) {
+	r.traced.Delete(id)
+	r.counts.Delete(id)
 }
 
-func (m *splitRegionCountManager) register(
-	cfID common.ChangeFeedID,
-	interval time.Duration,
-	checker *SplitSpanChecker,
-) func() {
-	if interval <= 0 {
-		interval = defaultRegionCountRefreshInterval
-	}
-
-	m.mu.Lock()
-	defer m.mu.Unlock()
-
-	key := cfID.String()
-	worker, ok := m.workers[key]
+func (r *regionCountRefresher) getRegionCount(id common.DispatcherID) (int, bool) {
+	value, ok := r.counts.Load(id)
 	if !ok {
-		worker = newSplitRegionCountWorker(cfID, interval)
-		m.workers[key] = worker
+		return 0, false
 	}
-	worker.addChecker(checker)
-	return func() {
-		m.unregister(cfID, checker)
-	}
+	return value.(int), true
 }
 
-func (m *splitRegionCountManager) unregister(cfID common.ChangeFeedID, checker *SplitSpanChecker) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-
-	key := cfID.String()
-	worker, ok := m.workers[key]
-	if !ok {
-		return
-	}
-	worker.removeChecker(checker)
-	if worker.isEmpty() {
-		worker.stop()
-		delete(m.workers, key)
-	}
-}
-
-type splitRegionCountWorker struct {
-	cfID     common.ChangeFeedID
-	mu       sync.Mutex
-	checkers map[*SplitSpanChecker]struct{}
-	interval time.Duration
-
-	stopCh    chan struct{}
-	stoppedCh chan struct{}
-}
-
-func newSplitRegionCountWorker(cfID common.ChangeFeedID, interval time.Duration) *splitRegionCountWorker {
-	worker := &splitRegionCountWorker{
-		cfID:      cfID,
-		checkers:  make(map[*SplitSpanChecker]struct{}),
-		interval:  interval,
-		stopCh:    make(chan struct{}),
-		stoppedCh: make(chan struct{}),
-	}
-	go worker.run()
-	return worker
-}
-
-func (w *splitRegionCountWorker) run() {
-	ticker := time.NewTicker(w.interval)
+func (r *regionCountRefresher) refreshRegionCounts(
+	ctx context.Context,
+) {
+	ticker := time.NewTicker(r.interval)
 	defer ticker.Stop()
-	w.refreshAll()
 	for {
 		select {
-		case <-ticker.C:
-			w.refreshAll()
-		case <-w.stopCh:
-			close(w.stoppedCh)
+		case <-ctx.Done():
 			return
+		case <-ticker.C:
+			r.queryRegionCount(ctx)
 		}
 	}
 }
 
-func (w *splitRegionCountWorker) refreshAll() {
-	checkers := w.snapshotCheckers()
-	if len(checkers) == 0 {
-		return
-	}
-	for _, checker := range checkers {
-		checker.refreshRegionCounts()
-	}
-}
+func (r *regionCountRefresher) queryRegionCount(ctx context.Context) {
+	backoff := tikv.NewBackoffer(ctx, 2000)
 
-func (w *splitRegionCountWorker) snapshotCheckers() []*SplitSpanChecker {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	if len(w.checkers) == 0 {
-		return nil
-	}
-	res := make([]*SplitSpanChecker, 0, len(w.checkers))
-	for checker := range w.checkers {
-		res = append(res, checker)
-	}
-	return res
-}
-
-func (w *splitRegionCountWorker) addChecker(checker *SplitSpanChecker) {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	w.checkers[checker] = struct{}{}
-}
-
-func (w *splitRegionCountWorker) removeChecker(checker *SplitSpanChecker) {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	delete(w.checkers, checker)
-}
-
-func (w *splitRegionCountWorker) isEmpty() bool {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	return len(w.checkers) == 0
-}
-
-func (w *splitRegionCountWorker) stop() {
-	close(w.stopCh)
-	<-w.stoppedCh
+	r.traced.Range(func(key, value any) bool {
+		dispatcherID := key.(common.DispatcherID)
+		span := value.(*heartbeatpb.TableSpan)
+		regions, err := r.regionCache.LoadRegionsInKeyRange(
+			backoff,
+			span.StartKey,
+			span.EndKey,
+		)
+		if err != nil {
+			log.Warn("load regions failed, just continue",
+				zap.Stringer("dispatcerID", dispatcherID),
+				zap.String("span", common.FormatTableSpan(span)),
+				zap.Error(err))
+			return true
+		}
+		r.counts.Store(dispatcherID, len(regions))
+		return true
+	})
 }
