1d15ee8f8298729e5aaec3963a8a773b1b839d1c
wk989898 <nhsmwk@gmail.com>
2025-12-22 04:52:32 +0000

    debug

--- downstreamadapter/dispatchermanager/dispatcher_manager.go
+++ downstreamadapter/dispatchermanager/dispatcher_manager.go
@@ -617,6 +617,7 @@ func (e *DispatcherManager) collectComponentStatusWhenChanged(ctx context.Contex
 				newRedoWatermark.Seq = tableSpanStatus.Seq
 				if tableSpanStatus.CheckpointTs != 0 && tableSpanStatus.CheckpointTs < newRedoWatermark.CheckpointTs {
 					newRedoWatermark.CheckpointTs = tableSpanStatus.CheckpointTs
+					log.Error("redoDispatcherMap collectComponentStatusWhenChanged", zap.Any("id", tableSpanStatus.ID), zap.Any("watermark", newRedoWatermark))
 				}
 			}
 			delay.Reset(10 * time.Millisecond)
@@ -638,6 +639,7 @@ func (e *DispatcherManager) collectComponentStatusWhenChanged(ctx context.Contex
 						}
 						if tableSpanStatus.CheckpointTs != 0 && tableSpanStatus.CheckpointTs < newRedoWatermark.CheckpointTs {
 							newRedoWatermark.CheckpointTs = tableSpanStatus.CheckpointTs
+							log.Error("redoDispatcherMap collectComponentStatusWhenChanged", zap.Any("id", tableSpanStatus.ID), zap.Any("watermark", newRedoWatermark))
 						}
 					}
 				case <-delay.C:
@@ -710,6 +712,7 @@ func (e *DispatcherManager) aggregateDispatcherHeartbeats(needCompleteStatus boo
 					redoDispatchersWithoutWatermark = append(redoDispatchersWithoutWatermark, id)
 				}
 			}
+			log.Error("redoDispatcherMap", zap.Any("id", id), zap.Any("watermark", watermark))
 		})
 		message.RedoWatermark.Seq = redoSeq
 		e.latestRedoWatermark.Set(message.RedoWatermark)
