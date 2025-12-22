f13b257cf95604244fef4fcb25144d894fe21eb7
hongyunyan <649330952@qq.com>
2025-12-22 22:16:23 +0800

    update

--- logservice/schemastore/validator.go
+++ logservice/schemastore/validator.go
@@ -14,18 +14,17 @@
 package schemastore
 
 import (
+	"context"
 	"encoding/json"
 	"strings"
-	"time"
+	"sync"
 
 	"github.com/pingcap/errors"
-	"github.com/pingcap/log"
 	"github.com/pingcap/ticdc/pkg/common"
 	cerror "github.com/pingcap/ticdc/pkg/errors"
 	"github.com/pingcap/ticdc/pkg/filter"
 	tidbkv "github.com/pingcap/tidb/pkg/kv"
 	timodel "github.com/pingcap/tidb/pkg/meta/model"
-	"go.uber.org/zap"
 )
 
 // VerifyTables catalog tables specified by ReplicaConfig into
@@ -33,69 +32,172 @@ import (
 func VerifyTables(f filter.Filter, storage tidbkv.Storage, startTs uint64) (
 	[]*common.TableInfo, []string, []string, error,
 ) {
+	// NOTE: We keep a fixed number of workers here. The goal is to parallelize the
+	// JSON unmarshal of timodel.TableInfo while avoiding excessive goroutines and
+	// memory pressure for huge table counts.
+	const (
+		tableWorkers = 16
+		batchSize    = 1024
+	)
+
 	meta := getSnapshotMeta(storage, startTs)
 	dbinfos, err := meta.ListDatabases()
 	if err != nil {
 		return nil, nil, nil, cerror.WrapError(cerror.ErrMetaListDatabases, err)
 	}
+
 	tableInfos := make([]*common.TableInfo, 0)
 	ineligibleTables := make([]string, 0)
 	eligibleTables := make([]string, 0)
+
+	type tableTask struct {
+		schema string
+		values [][]byte
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var (
+		appendMu sync.Mutex
+		taskWg   sync.WaitGroup
+		workerWg sync.WaitGroup
+
+		errOnce  sync.Once
+		firstErr error
+	)
+
+	setErr := func(err error) {
+		if err == nil {
+			return
+		}
+		errOnce.Do(func() {
+			firstErr = err
+			cancel()
+		})
+	}
+
+	tasks := make(chan tableTask, tableWorkers*2)
+
+	worker := func() {
+		defer workerWg.Done()
+		for task := range tasks {
+			func() {
+				defer taskWg.Done()
+
+				if ctx.Err() != nil {
+					return
+				}
+
+				localInfos := make([]*common.TableInfo, 0, len(task.values))
+				localIneligible := make([]string, 0)
+				localEligible := make([]string, 0)
+
+				for _, value := range task.values {
+					if ctx.Err() != nil {
+						return
+					}
+
+					tbInfo := &timodel.TableInfo{}
+					if err := json.Unmarshal(value, tbInfo); err != nil {
+						setErr(errors.Trace(err))
+						return
+					}
+
+					tableName := tbInfo.Name.O
+					if f.ShouldIgnoreTable(task.schema, tableName) {
+						continue
+					}
+					// Sequence is not supported yet, TiCDC needs to filter all sequence tables.
+					// See https://github.com/pingcap/tiflow/issues/4559
+					if tbInfo.Sequence != nil {
+						continue
+					}
+
+					tableInfo := common.WrapTableInfo(task.schema, tbInfo)
+					localInfos = append(localInfos, tableInfo)
+					if !tableInfo.IsEligible(false /* forceReplicate */) {
+						localIneligible = append(localIneligible, tableInfo.GetTableName())
+					} else {
+						localEligible = append(localEligible, tableInfo.GetTableName())
+					}
+				}
+
+				if len(localInfos) == 0 {
+					return
+				}
+
+				appendMu.Lock()
+				tableInfos = append(tableInfos, localInfos...)
+				ineligibleTables = append(ineligibleTables, localIneligible...)
+				eligibleTables = append(eligibleTables, localEligible...)
+				appendMu.Unlock()
+			}()
+		}
+	}
+
+	workerWg.Add(tableWorkers)
+	for i := 0; i < tableWorkers; i++ {
+		go worker()
+	}
+
+	sendTask := func(task tableTask) bool {
+		if len(task.values) == 0 {
+			return true
+		}
+		taskWg.Add(1)
+		select {
+		case tasks <- task:
+			return true
+		case <-ctx.Done():
+			taskWg.Done()
+			return false
+		}
+	}
+
 	for _, dbinfo := range dbinfos {
-		start := time.Now()
+		if ctx.Err() != nil {
+			break
+		}
 		if f.ShouldIgnoreSchema(dbinfo.Name.O) {
-			log.Debug("ignore database", zap.Stringer("db", dbinfo.Name))
 			continue
 		}
 
 		rawTables, err := meta.GetMetasByDBID(dbinfo.ID)
 		if err != nil {
-			return nil, nil, nil, cerror.WrapError(cerror.ErrMetaListDatabases, err)
+			setErr(cerror.WrapError(cerror.ErrMetaListDatabases, err))
+			break
 		}
-		log.Info("get rawTables", zap.Any("timeSpent", time.Since(start)), zap.Int("tableCount", len(rawTables)))
+
+		batch := make([][]byte, 0, batchSize)
 		for _, r := range rawTables {
-			start = time.Now()
-			tableKey := string(r.Field)
-			if !strings.HasPrefix(tableKey, mTablePrefix) {
+			if ctx.Err() != nil {
+				break
+			}
+			if !strings.HasPrefix(string(r.Field), mTablePrefix) {
 				continue
 			}
-
-			tbName := &timodel.TableNameInfo{}
-			err := json.Unmarshal(r.Value, tbName)
-			if err != nil {
-				return nil, nil, nil, errors.Trace(err)
+			batch = append(batch, r.Value)
+			if len(batch) >= batchSize {
+				if !sendTask(tableTask{schema: dbinfo.Name.O, values: batch}) {
+					break
+				}
+				batch = make([][]byte, 0, batchSize)
 			}
-
-			log.Info("unmarshal table name", zap.Any("timeSpent", time.Since(start)))
-
-			tbInfo := &timodel.TableInfo{}
-			err = json.Unmarshal(r.Value, tbInfo)
-			if err != nil {
-				return nil, nil, nil, errors.Trace(err)
-			}
-
-			log.Info("unmarshal table info", zap.Any("timeSpent", time.Since(start)))
-			tableInfo := common.WrapTableInfo(dbinfo.Name.O, tbInfo)
-			log.Info("wrap table info", zap.Any("timeSpent", time.Since(start)))
-			if f.ShouldIgnoreTable(dbinfo.Name.O, tbName.Name.O) {
-				log.Debug("ignore table", zap.String("db", dbinfo.Name.O),
-					zap.String("table", tbName.Name.O))
-				continue
-			}
-			// Sequence is not supported yet, TiCDC needs to filter all sequence tables.
-			// See https://github.com/pingcap/tiflow/issues/4559
-			if tableInfo.IsSequence() {
-				continue
-			}
-			tableInfos = append(tableInfos, tableInfo)
-			if !tableInfo.IsEligible(false /* forceReplicate */) {
-				ineligibleTables = append(ineligibleTables, tableInfo.GetTableName())
-			} else {
-				eligibleTables = append(eligibleTables, tableInfo.GetTableName())
-			}
-			log.Info("check table eligible", zap.Any("timeSpent", time.Since(start)))
 		}
+
+		if ctx.Err() == nil {
+			_ = sendTask(tableTask{schema: dbinfo.Name.O, values: batch})
+		}
+
+		taskWg.Wait()
 	}
 
+	close(tasks)
+	workerWg.Wait()
+
+	if firstErr != nil {
+		return nil, nil, nil, firstErr
+	}
 	return tableInfos, ineligibleTables, eligibleTables, nil
 }
