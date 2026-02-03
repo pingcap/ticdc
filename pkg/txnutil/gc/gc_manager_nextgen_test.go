1217e5ed22d0337bc50eea1efd5995abc60074aa
wk989898 <nhsmwk@gmail.com>
2026-02-03 14:04:09 +0000

    fix ut
    Signed-off-by: wk989898 <nhsmwk@gmail.com>

--- pkg/txnutil/gc/gc_manager_nextgen_test.go
+++ pkg/txnutil/gc/gc_manager_nextgen_test.go
@@ -22,7 +22,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/pingcap/errors"
 	"github.com/pingcap/ticdc/pkg/common"
 	appcontext "github.com/pingcap/ticdc/pkg/common/context"
 	cerrors "github.com/pingcap/ticdc/pkg/errors"
@@ -30,30 +29,6 @@ import (
 	"github.com/stretchr/testify/require"
 	pdgc "github.com/tikv/pd/client/clients/gc"
 )
-
-type mockGCStatesClient struct {
-	txnSafePoint uint64
-}
-
-func (c *mockGCStatesClient) SetGCBarrier(
-	ctx context.Context, barrierID string, barrierTS uint64, ttl time.Duration,
-) (*pdgc.GCBarrierInfo, error) {
-	if barrierTS < c.txnSafePoint {
-		// Mark this error as non-retryable for SetGCBarrier's internal retry loop.
-		return nil, errors.Annotate(context.Canceled, "ErrGCBarrierTSBehindTxnSafePoint")
-	}
-	return pdgc.NewGCBarrierInfo(barrierID, barrierTS, ttl, time.Now()), nil
-}
-
-func (c *mockGCStatesClient) DeleteGCBarrier(ctx context.Context, barrierID string) (*pdgc.GCBarrierInfo, error) {
-	return nil, nil
-}
-
-func (c *mockGCStatesClient) GetGCState(ctx context.Context) (pdgc.GCState, error) {
-	return pdgc.GCState{
-		TxnSafePoint: c.txnSafePoint,
-	}, nil
-}
 
 func TestTryUpdateKeyspaceGCBarrierDoesNotReturnSnapshotLost(t *testing.T) {
 	appcontext.SetService(appcontext.DefaultPDClock, pdutil.NewClock4Test())
