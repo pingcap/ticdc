192feeb119af01fc62d34ef60e4ef2118ff8948a
nhsmw <nhsmwk@gmail.com>
2026-02-10 16:26:16 +0800

    api: make api compatible with old arch (#4075)
    ref pingcap/ticdc#3888

--- tests/integration_tests/http_api_tls/util/test_case.py
+++ tests/integration_tests/http_api_tls/util/test_case.py
@@ -208,17 +208,31 @@ def update_changefeed():
     # update fail
     # can only update a stopped changefeed
     url = BASE_URL0 + "/changefeeds/changefeed-test1?keyspace=keyspace1"
-    data = json.dumps({"mounter_worker_num": 32})
+    data = json.dumps({
+        "replica_config": {
+            "ignore_ineligible_table": True,
+            "mounter": {
+                "worker_num": 32
+            }
+        }
+    })
     headers = {"Content-Type": "application/json"}
     resp = rq.put(url, data=data, headers=headers, cert=CERT, verify=VERIFY)
     assert resp.status_code == rq.codes.bad_request
 
     # update success
     url = BASE_URL0 + "/changefeeds/changefeed-test2?keyspace=keyspace1"
-    data = json.dumps({"mounter_worker_num": 32})
+    data = json.dumps({
+        "replica_config": {
+            "ignore_ineligible_table": True,
+            "mounter": {
+                "worker_num": 32
+            }
+        }
+    })
     headers = {"Content-Type": "application/json"}
     resp = rq.put(url, data=data, headers=headers, cert=CERT, verify=VERIFY)
-    assert resp.status_code == rq.codes.ok
+    assert resp.status_code == rq.codes.ok, f"resp: {resp.text}"
 
     # update fail
     # can't update start_ts
@@ -255,7 +269,9 @@ def update_changefeed():
 def resume_changefeed():
     # resume changefeed
     url = BASE_URL1 + "/changefeeds/changefeed-test2/resume?keyspace=keyspace1"
-    resp = rq.post(url, cert=CERT, verify=VERIFY)
+    data = json.dumps({})
+    headers = {"Content-Type": "application/json"}
+    resp = rq.post(url, data=data, headers=headers, cert=CERT, verify=VERIFY)
     assert resp.status_code == rq.codes.ok
 
     # check if resume changefeed success
