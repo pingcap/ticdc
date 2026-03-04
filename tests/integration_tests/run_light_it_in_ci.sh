6e95dc75a32ce4f5769942b8dc0c8307864059b2
wk989898 <nhsmwk@gmail.com>
2026-03-04 07:12:09 +0000

    update
    Signed-off-by: wk989898 <nhsmwk@gmail.com>

--- tests/integration_tests/run_light_it_in_ci.sh
+++ tests/integration_tests/run_light_it_in_ci.sh
@@ -50,22 +50,13 @@ mysql_groups=(
 	# G08
 	'capture_session_done_during_task changefeed_dup_error_restart mysql_sink_retry fail_over_ddl_I'
 	# G09
-<<<<<<< HEAD
 	'sequence cdc_server_tips ddl_sequence server_config_compatibility log_redaction fail_over_ddl_J'
-=======
-	'sequence cdc_server_tips ddl_sequence server_config_compatibility fail_over_ddl_J'
->>>>>>> 02cab93ee (test: add more integration test cases (#3176))
 	# G10
 	'overwrite_resume_with_syncpoint restart_changefeed changefeed_error bdr_mode fail_over_ddl_K split_table_check'
 	# G11
 	'kill_owner_with_ddl multi_tables_ddl ddl_attributes multi_cdc_cluster fail_over_ddl_L'
 	# G12
-<<<<<<< HEAD
 	'row_format tiflash multi_rocks fail_over_ddl_M correctness_for_shared_column_schema'
-=======
-	# ds_memory_control
-	'row_format tiflash multi_rocks fail_over_ddl_M'
->>>>>>> 02cab93ee (test: add more integration test cases (#3176))
 	# G13
 	'cli_tls_with_auth cli_with_auth fail_over_ddl_N maintainer_failover_when_operator'
 	# G14
