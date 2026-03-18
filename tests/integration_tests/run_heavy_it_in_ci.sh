f7e3fd7a5d738f9da96e8e2e6041700fe591d42a
hongyunyan <649330952@qq.com>
2026-03-19 07:07:59 +0800

    update

--- tests/integration_tests/run_heavy_it_in_ci.sh
+++ tests/integration_tests/run_heavy_it_in_ci.sh
@@ -30,37 +30,38 @@ group_num=${group#G}
 # 12 CPU cores will be allocated to run each mysql heavy group in CI pipelines.
 mysql_groups=(
 	# G00
-	'api_v2 generate_column many_pk_or_uk multi_source'
+	'random_drop_message random_drop_message random_drop_message'
 	# G01
-	'ddl_for_split_tables_with_random_move_table'
+	'random_drop_message random_drop_message random_drop_message'
 	# G02
-	'ddl_for_split_tables_with_failover'
+	'random_drop_message random_drop_message random_drop_message'
 	# G03
-	'cdc move_table in_flight_ddl_during_scheduling checkpoint_race_ddl_crash'
+	'random_drop_message random_drop_message random_drop_message'
 	# G04
-	'complex_transaction'
+	'random_drop_message random_drop_message random_drop_message'
 	# G05
-	'ddl_for_split_tables_with_merge_and_split syncpoint in_flight_syncpoint_during_scheduling syncpoint_check_ts random_drop_message'
+	'random_drop_message random_drop_message random_drop_message'
 	# G06
-	'ddl_for_split_tables_with_random_merge_and_split'
-	# G07
-	'consistent_compatibility consistent_partition_table consistent_replicate_gbk consistent_replicate_ddl consistent_replicate_basic'
-	# G08
-	'default_value http_proxies bank ddl_for_split_tables_random_schedule'
-	# G09
-	'availability resolve_lock merge_table drop_many_tables ddl_for_split_tables'
-	# G10
-	'consistent_replicate_nfs consistent_replicate_storage_file consistent_replicate_storage_file_large_value consistent_replicate_storage_s3'
-	# G11
-	'multi_changefeeds ddl_wait ddl_reentrant force_replicate_table multi_source'
-	# G12
-	'ddl_with_random_move_table'
+	'random_drop_message random_drop_message random_drop_message'
+	# G01
+	'random_drop_message random_drop_message random_drop_message'
+	# G02
+	'random_drop_message random_drop_message random_drop_message'
+	# G03
+	'random_drop_message random_drop_message random_drop_message'
+	# G04
+	'random_drop_message random_drop_message random_drop_message'
+	# G05
+	'random_drop_message random_drop_message random_drop_message'
 	# G13
-	'tidb_mysql_test fail_over region_merge fail_over_ddl_mix_random_delay'
-	# G14
-	'fail_over_ddl_mix'
-	# G15
-	'fail_over_ddl_mix_with_syncpoint'
+	'random_drop_message random_drop_message random_drop_message'
+	# G01
+	'random_drop_message random_drop_message random_drop_message'
+	# G02
+	'random_drop_message random_drop_message random_drop_message'
+	# G03
+	'random_drop_message random_drop_message random_drop_message'
+	# G04
 )
 
 # 12 CPU cores will be allocated to run each kafka heavy group in CI pipelines.
