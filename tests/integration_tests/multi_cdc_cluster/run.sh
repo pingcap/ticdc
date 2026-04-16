bcbfe8c06418dafe1d17598642b7acc8ec6d7137
nhsmw <nhsmwk@gmail.com>
2026-04-17 01:36:57 +0800

    etcd: fix cluster-id prefix collision (#4811)
    close pingcap/ticdc#4756

--- tests/integration_tests/multi_cdc_cluster/run.sh
+++ tests/integration_tests/multi_cdc_cluster/run.sh
@@ -26,12 +26,22 @@ function run() {
 	# run one cdc cluster
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --cluster-id "test1" --addr "127.0.0.1:8300" --logsuffix mult_cdc.server1
 	# run another cdc cluster
-	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --cluster-id "test2" --addr "127.0.0.1:8301" --logsuffix mult_cdc.server2
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --cluster-id "test11" --addr "127.0.0.1:8301" --logsuffix mult_cdc.server2
 
 	SINK_URI="mysql://normal:123456@127.0.0.1:3306/"
 
-	cdc_cli_changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" --server "http://127.0.0.1:8300" --config="$CUR/conf/changefeed1.toml"
-	cdc_cli_changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" --server "http://127.0.0.1:8301" --config="$CUR/conf/changefeed2.toml"
+	cdc_cli_changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" --server "http://127.0.0.1:8300" --config="$CUR/conf/changefeed1.toml" --changefeed-id "test1"
+	cdc_cli_changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" --server "http://127.0.0.1:8301" --config="$CUR/conf/changefeed2.toml" --changefeed-id "test2"
+
+	ensure 20 "curl --silent --show-error --fail --user ticdc:ticdc_secret 'http://127.0.0.1:8300/api/v2/changefeeds?keyspace=${KEYSPACE_NAME}' | jq -e '.total == 1 and .items[0].id == \"test1\"' >/dev/null"
+	ensure 20 "curl --silent --show-error --fail --user ticdc:ticdc_secret 'http://127.0.0.1:8301/api/v2/changefeeds?keyspace=${KEYSPACE_NAME}' | jq -e '.total == 1 and .items[0].id == \"test2\"' >/dev/null"
+
+	cdc_pid_1=$(get_cdc_pid 127.0.0.1 8300)
+	kill_cdc_pid $cdc_pid_1
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --cluster-id "test1" --addr "127.0.0.1:8300" --logsuffix mult_cdc.server1
+
+	ensure 20 "curl --silent --show-error --fail --user ticdc:ticdc_secret 'http://127.0.0.1:8300/api/v2/changefeeds?keyspace=${KEYSPACE_NAME}' | jq -e '.total == 1 and .items[0].id == \"test1\"' >/dev/null"
+	ensure 20 "curl --silent --show-error --fail --user ticdc:ticdc_secret 'http://127.0.0.1:8301/api/v2/changefeeds?keyspace=${KEYSPACE_NAME}' | jq -e '.total == 1 and .items[0].id == \"test2\"' >/dev/null"
 
 	# same dml for table multi_cdc1
 	run_sql "INSERT INTO test.multi_cdc1(id, val) VALUES (1, 1);"
