b1057f0b11d53f7f6591a348cb35b28efbc4e209
dongmen <414110582@qq.com>
2026-04-23 13:04:30 +0800

    maintainer: adjust log
    Signed-off-by: dongmen <414110582@qq.com>

--- maintainer/maintainer.go
+++ maintainer/maintainer.go
@@ -308,7 +308,10 @@ func (m *Maintainer) HandleEvent(event *Event) bool {
 					zap.Stringer("changefeedID", m.changefeedID),
 					zap.Int("eventType", event.eventType),
 					zap.Duration("duration", duration),
-					zap.Any("Message", event.message),
+					zap.String("from", event.message.From.String()),
+					zap.String("to", event.message.To.String()),
+					zap.String("type", event.message.Type.String()),
+					zap.String("topic", event.message.Topic),
 				)
 			} else {
 				log.Info("maintainer is too slow",
