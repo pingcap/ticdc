ab5dba76527fc48a3b72c10f5cd745634c32dd12
lidezhu <lidezhu@pingcap.com>
2026-04-26 22:13:29 +0800

    f

--- logservice/eventstore/event_store_bench_test.go
+++ logservice/eventstore/event_store_bench_test.go
@@ -25,20 +25,111 @@ import (
 	"github.com/pingcap/ticdc/pkg/common"
 )
 
+const benchmarkKiB = 1024
+
+var benchmarkWriteEventSizes = []struct {
+	name string
+	size int
+}{
+	{name: "4K", size: 4 * benchmarkKiB},
+	{name: "8K", size: 8 * benchmarkKiB},
+	{name: "16K", size: 16 * benchmarkKiB},
+	{name: "32K", size: 32 * benchmarkKiB},
+}
+
+var benchmarkCompressionModes = []struct {
+	name    string
+	enabled bool
+}{
+	{name: "CompressionOff", enabled: false},
+	{name: "CompressionOn", enabled: true},
+}
+
+func makeBenchmarkRawKVEntry(index int, valueSize int) common.RawKVEntry {
+	key := []byte(fmt.Sprintf("key-%06d", index))
+	value := bytes.Repeat([]byte{'v'}, valueSize)
+	return common.RawKVEntry{
+		OpType:  common.OpTypePut,
+		StartTs: uint64(100 + index),
+		CRTs:    uint64(1000 + index),
+		Key:     key,
+		Value:   value,
+	}
+}
+
 func makeBenchmarkRawKVEntries(count int, valueSize int) []common.RawKVEntry {
 	kvs := make([]common.RawKVEntry, 0, count)
 	for i := 0; i < count; i++ {
-		key := []byte(fmt.Sprintf("key-%06d", i))
-		value := bytes.Repeat([]byte{'v'}, valueSize)
-		kvs = append(kvs, common.RawKVEntry{
-			OpType:  common.OpTypePut,
-			StartTs: uint64(100 + i),
-			CRTs:    uint64(1000 + i),
-			Key:     key,
-			Value:   value,
-		})
+		kvs = append(kvs, makeBenchmarkRawKVEntry(i, valueSize))
 	}
 	return kvs
+}
+
+func makeBenchmarkWriteRawKVEntries(targetSize int) []common.RawKVEntry {
+	entry := makeBenchmarkRawKVEntry(0, 1)
+	fixedSize := int(entry.GetSize()) - len(entry.Value)
+	valueSize := targetSize - fixedSize
+	if valueSize < 1 {
+		valueSize = 1
+	}
+	return []common.RawKVEntry{makeBenchmarkRawKVEntry(0, valueSize)}
+}
+
+func BenchmarkEventStoreWriteEvents(b *testing.B) {
+	for _, compressionMode := range benchmarkCompressionModes {
+		b.Run(compressionMode.name, func(b *testing.B) {
+			for _, size := range benchmarkWriteEventSizes {
+				b.Run(size.name, func(b *testing.B) {
+					_, storeInt := newEventStoreForTest(b.TempDir())
+					store := storeInt.(*eventStore)
+					store.enableZstdCompression = compressionMode.enabled
+					defer store.Close(context.Background())
+
+					events := []eventWithCallback{{
+						subID:    1,
+						tableID:  1,
+						kvs:      makeBenchmarkWriteRawKVEntries(size.size),
+						callback: func() {},
+					}}
+					if compressionMode.enabled &&
+						size.name == "32K" &&
+						!hasCompressibleBenchmarkKV(store, events) {
+						b.Fatalf("32K case should trigger compression, threshold=%d", store.compressionThreshold)
+					}
+
+					encoder, err := zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.SpeedFastest))
+					if err != nil {
+						b.Fatal(err)
+					}
+					defer encoder.Close()
+					var compressionBuf []byte
+					var rawValueBuf []byte
+
+					b.ReportAllocs()
+					b.ResetTimer()
+					for i := 0; i < b.N; i++ {
+						if err := store.writeEvents(store.dbs[0], events, encoder, &compressionBuf, &rawValueBuf); err != nil {
+							b.Fatal(err)
+						}
+					}
+				})
+			}
+		})
+	}
+}
+
+func hasCompressibleBenchmarkKV(store *eventStore, events []eventWithCallback) bool {
+	if !store.enableZstdCompression {
+		return false
+	}
+	for _, event := range events {
+		for i := range event.kvs {
+			if event.kvs[i].GetSize() > int64(store.compressionThreshold) {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func BenchmarkEventWithCallbackSizer(b *testing.B) {
@@ -52,33 +143,6 @@ func BenchmarkEventWithCallbackSizer(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = eventWithCallbackSizer(event)
-	}
-}
-
-func BenchmarkEventStoreWriteEvents(b *testing.B) {
-	_, storeInt := newEventStoreForTest(b.TempDir())
-	store := storeInt.(*eventStore)
-	defer store.Close(context.Background())
-
-	events := []eventWithCallback{{
-		subID:    1,
-		tableID:  1,
-		kvs:      makeBenchmarkRawKVEntries(128, 256),
-		callback: func() {},
-	}}
-	encoder, err := zstd.NewWriter(nil)
-	if err != nil {
-		b.Fatal(err)
-	}
-	defer encoder.Close()
-	var compressionBuf []byte
-
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		if err := store.writeEvents(store.dbs[0], events, encoder, &compressionBuf); err != nil {
-			b.Fatal(err)
-		}
 	}
 }
 
@@ -107,7 +171,8 @@ func BenchmarkEventStoreIteratorNext(b *testing.B) {
 	}
 	defer encoder.Close()
 	var compressionBuf []byte
-	if err := store.writeEvents(db, events, encoder, &compressionBuf); err != nil {
+	var rawValueBuf []byte
+	if err := store.writeEvents(db, events, encoder, &compressionBuf, &rawValueBuf); err != nil {
 		b.Fatal(err)
 	}
 
