6a7feddad3a484791327739a4660081ebfe953cf
wk989898 <nhsmwk@gmail.com>
2026-06-09 07:28:43 +0000

    update
    Signed-off-by: wk989898 <nhsmwk@gmail.com>

--- downstreamadapter/dispatcher/basic_dispatcher.go
+++ downstreamadapter/dispatcher/basic_dispatcher.go
@@ -524,10 +524,7 @@ func (d *BasicDispatcher) handleEvents(dispatcherEvents []DispatcherEvent, wakeC
 			log.Info("dispatcher receive ddl event",
 				zap.Stringer("dispatcher", d.id),
 				zap.String("query", ddl.Query),
-<<<<<<< HEAD
-=======
 				zap.Any("tableSpan", d.GetTableSpan()),
->>>>>>> 019e99244 (redo: add more information and metrics (#4461))
 				zap.Int64("table", ddl.GetTableID()),
 				zap.Uint64("commitTs", event.GetCommitTs()),
 				zap.Uint64("seq", event.GetSeq()))
