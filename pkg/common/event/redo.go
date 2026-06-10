f679533ee9a6eb5aa97379fbcddd5545fd113238
nhsmw <nhsmwk@gmail.com>
2026-06-10 11:12:22 +0800

    Update redo.go

--- pkg/common/event/redo.go
+++ pkg/common/event/redo.go
@@ -21,6 +21,7 @@ import (
 	"github.com/pingcap/ticdc/pkg/util"
 	timodel "github.com/pingcap/tidb/pkg/meta/model"
 	parser_model "github.com/pingcap/tidb/pkg/parser/model"
+	"github.com/pingcap/tidb/pkg/parser/ast"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
 	tiTypes "github.com/pingcap/tidb/pkg/types"
 	"github.com/pingcap/tidb/pkg/util/chunk"
