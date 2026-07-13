33f60bb2daf4881a5403418c109a7dcac1aeaf53
dongmen <414110582@qq.com>
2026-07-13 16:55:23 +0800

    eventservice: shorten low latency resolved ts flush

--- pkg/eventservice/event_broker.go
+++ pkg/eventservice/event_broker.go
@@ -44,8 +44,9 @@ const (
 	resolvedTsCacheSize = 512
 	basicChannelSize    = 2048
 
-	defaultMaxBatchSize            = 128
-	defaultFlushResolvedTsInterval = 25 * time.Millisecond
+	defaultMaxBatchSize               = 128
+	defaultFlushResolvedTsInterval    = 25 * time.Millisecond
+	lowLatencyFlushResolvedTsInterval = 5 * time.Millisecond
 
 	defaultReportDispatcherStatToStoreInterval = time.Second * 10
 
@@ -796,7 +797,7 @@ func releaseQuota(quota *atomic.Uint64, nBytes uint64) {
 }
 
 func (c *eventBroker) runSendMessageWorker(ctx context.Context, workerIndex int, topic string) error {
-	ticker := time.NewTicker(defaultFlushResolvedTsInterval)
+	ticker := time.NewTicker(flushResolvedTsInterval())
 	defer ticker.Stop()
 
 	resolvedTsCacheMap := make(map[node.ID]*resolvedTsCache)
@@ -845,6 +846,13 @@ func (c *eventBroker) runSendMessageWorker(ctx context.Context, workerIndex int,
 			}
 		}
 	}
+}
+
+func flushResolvedTsInterval() time.Duration {
+	if config.GetGlobalServerConfig().IsLowLatencyMode() {
+		return lowLatencyFlushResolvedTsInterval
+	}
+	return defaultFlushResolvedTsInterval
 }
 
 func (c *eventBroker) handleResolvedTs(ctx context.Context, cacheMap map[node.ID]*resolvedTsCache, m *wrapEvent, workerIndex int, topic string) {
