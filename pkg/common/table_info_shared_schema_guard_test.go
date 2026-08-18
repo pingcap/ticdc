1d0176a1141f39cc878104257e938b8f262a93ee
nhsmw <nhsmwk@gmail.com>
2026-08-18 11:23:10 +0800

    Remove unused fields from shared schema tests

--- pkg/common/table_info_shared_schema_guard_test.go
+++ pkg/common/table_info_shared_schema_guard_test.go
@@ -79,13 +79,13 @@ func TestLatestTiDBTableInfoSharedSchemaGuard(t *testing.T) {
 				"PKIsHandle", "IsCommonHandle", "CommonHandleVersion",
 				"Comment", "AutoIncID", "AutoIncIDExtra", "AutoIDCache", "AutoRandID",
 				"MaxColumnID", "MaxIndexID", "MaxForeignKeyID", "MaxConstraintID", "UpdateTS", "AutoIDSchemaID",
-				"ShardRowIDBits", "MaxShardRowIDBits", "AutoRandomBits", "AutoRandomRangeBits", "PreSplitRegions", "TableSplitPolicy",
+				"ShardRowIDBits", "MaxShardRowIDBits", "AutoRandomBits", "AutoRandomRangeBits", "PreSplitRegions",
 				"Partition", "Compression", "View", "Sequence", "Lock", "Version", "TiFlashReplica", "IsColumnar",
 				"TempTableType", "TableCacheStatusType", "PlacementPolicyRef", "StatsOptions",
 				"ExchangePartitionInfo", "TTLInfo", "IsActiveActive", "SoftdeleteInfo", "Affinity",
 				"Revision", "DBID",
 				// These table-level storage settings do not affect the shared column schema.
-				"EngineAttribute", "StorageClassTier", "StorageClassTransitions", "Mode",
+				"Mode",
 			},
 		},
 		{
@@ -106,8 +106,8 @@ func TestLatestTiDBTableInfoSharedSchemaGuard(t *testing.T) {
 			typeName:        "IndexInfo",
 			expectedFields: []string{
 				"ID", "Name", "Table", "Columns", "State", "BackfillState", "Comment", "Tp", "Unique", "Primary",
-				"Invisible", "Global", "MVIndex", "VectorInfo", "InvertedInfo", "FullTextInfo",
-				"ConditionExprString", "AffectColumn", "RegionSplitPolicy", "GlobalIndexVersion",
+				"Invisible", "Global", "MVIndex", "VectorInfo",
+				"ConditionExprString", "AffectColumn", "GlobalIndexVersion",
 			},
 		},
 		{
