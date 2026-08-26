c196f2e1bd31eca2e4c64e811bd6e0c6cddff0b5
dongmen <414110582@qq.com>
2026-08-26 16:58:24 +0800

    messaging: test write lease response round trip

--- pkg/messaging/message_write_lease_test.go
+++ pkg/messaging/message_write_lease_test.go
@@ -14,12 +14,53 @@
 package messaging
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/pingcap/ticdc/heartbeatpb"
 	"github.com/pingcap/ticdc/pkg/node"
 	"github.com/stretchr/testify/require"
 )
+
+func TestNodeHeartbeatResponseRemoteRoundTrip(t *testing.T) {
+	sender, senderAddr, stopSender := NewMessageCenterForTest(t)
+	receiver, receiverAddr, stopReceiver := NewMessageCenterForTest(t)
+	t.Cleanup(stopSender)
+	t.Cleanup(stopReceiver)
+
+	sender.addTarget(receiver.id, receiverAddr)
+	receiver.addTarget(sender.id, senderAddr)
+	require.Eventually(t, func() bool {
+		return sender.IsReadyToSend(receiver.id) && receiver.IsReadyToSend(sender.id)
+	}, 10*time.Second, 100*time.Millisecond)
+
+	received := make(chan *TargetMessage, 1)
+	receiver.RegisterHandler(MaintainerManagerTopic, func(_ context.Context, message *TargetMessage) error {
+		received <- message
+		return nil
+	})
+
+	response := &heartbeatpb.NodeHeartbeatResponse{
+		CoordinatorVersion: 10,
+		TargetNodeEpoch:    11,
+		RequestSeq:         12,
+		LeaseDurationMs:    5000,
+	}
+	require.NoError(t, sender.SendCommand(
+		NewSingleTargetMessage(receiver.id, MaintainerManagerTopic, response),
+	))
+
+	select {
+	case message := <-received:
+		require.Equal(t, sender.id, message.From)
+		require.Equal(t, receiver.id, message.To)
+		require.Equal(t, TypeNodeHeartbeatResponse, message.Type)
+		require.Equal(t, response, message.Message[0])
+	case <-time.After(10 * time.Second):
+		t.Fatal("timed out waiting for node heartbeat response")
+	}
+}
 
 func TestNodeHeartbeatResponseIOTypeRoundTrip(t *testing.T) {
 	response := &heartbeatpb.NodeHeartbeatResponse{
