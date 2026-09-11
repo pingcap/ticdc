a62695e1b678137a41695199da826c261b8f7468
dongmen <20351731+asddongmen@users.noreply.github.com>
2026-09-11 11:02:43 +0800

    tests: reduce split-table move case resource pressure (#6211)
    close pingcap/ticdc#6210

--- tests/integration_tests/ddl_for_split_tables_with_random_move_table/data/pre.sql
+++ tests/integration_tests/ddl_for_split_tables_with_random_move_table/data/pre.sql
@@ -7,8 +7,8 @@ create table table_3 (id INT AUTO_INCREMENT PRIMARY KEY, data VARCHAR(255));
 create table table_4 (id INT AUTO_INCREMENT PRIMARY KEY, data VARCHAR(255));
 create table table_5 (id INT AUTO_INCREMENT PRIMARY KEY, data VARCHAR(255));
 
-split table test.table_1 between (1) and (100000) regions 50;
-split table test.table_2 between (1) and (100000) regions 50;
-split table test.table_3 between (1) and (100000) regions 50;
-split table test.table_4 between (1) and (100000) regions 50;
-split table test.table_5 between (1) and (100000) regions 50;
+split table test.table_1 between (1) and (100000) regions 20;
+split table test.table_2 between (1) and (100000) regions 20;
+split table test.table_3 between (1) and (100000) regions 20;
+split table test.table_4 between (1) and (100000) regions 20;
+split table test.table_5 between (1) and (100000) regions 20;
