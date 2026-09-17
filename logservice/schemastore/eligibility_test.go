5b470d4febf3b60836b7a5d9e1e98b7a87dc8aa9
wk989898 <nhsmwk@gmail.com>
2026-09-17 03:37:25 +0000

    fmt
    Signed-off-by: wk989898 <nhsmwk@gmail.com>

--- logservice/schemastore/eligibility_test.go
+++ logservice/schemastore/eligibility_test.go
@@ -16,6 +16,7 @@ package schemastore
 import (
 	"bytes"
 	"testing"
+	"testing/synctest"
 
 	"github.com/cockroachdb/pebble"
 	bf "github.com/pingcap/ticdc/pkg/binlog-filter"
@@ -28,7 +29,6 @@ import (
 	parser_model "github.com/pingcap/tidb/pkg/parser/model"
 	"github.com/stretchr/testify/require"
 	"github.com/tinylib/msgp/msgp"
-	"testing/synctest"
 )
 
 func TestPersistedReplicationKeyCompatibility(t *testing.T) {
