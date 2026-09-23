a55e2296898c43c2fef438b78d717d7f05455ead
lidezhu <lidezhu@pingcap.com>
2026-09-23 12:14:28 +0800

    eventstore: enable data sharing by default

--- pkg/config/debug.go
+++ pkg/config/debug.go
@@ -160,7 +160,7 @@ func NewDefaultEventStoreConfig() *EventStoreConfig {
 	return &EventStoreConfig{
 		CompressionThreshold:  16384, // 16KB
 		EnableZstdCompression: false,
-		EnableDataSharing:     false,
+		EnableDataSharing:     true,
 	}
 }
 
@@ -207,7 +207,7 @@ type EventServiceConfig struct {
 func NewDefaultEventServiceConfig() *EventServiceConfig {
 	return &EventServiceConfig{
 		ScanTaskQueueSize:        1024 * 8,
-		ScanLimitInBytes:         1024 * 1024 * 256, // 256MB
+		ScanLimitInBytes:         1024 * 1024 * 1024, // 1GB
 		DMLEventMaxRows:          256,
 		DMLEventMaxBytes:         1024 * 1024 * 1, // 1MB
 		LargeTxnThresholdInBytes: 1024 * 1024 * 1, // 1MB
